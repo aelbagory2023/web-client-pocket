f758319ad8eb50cdc7deeaaaed5dcd43bb0d9057
Joel Kelly <joel@getpocket.com>
2020-09-30 11:41:56 -0700

    refactor(topics): prelim static generation
    HOLY CRAP! So much faster.  Need to adjust some of the server side assumptions we are making but it will be worth it on a number of levels.  This is a vital example of the need to maintain the product as we move it forward.

--- src/pages/discover/[slug].js
+++ src/pages/discover/[slug].js
@@ -6,6 +6,7 @@ import { fetchSearchData } from 'containers/topic/topic.state'
 import { hydrateTopic } from 'containers/topic/topic.state'
 import { hydrateItems } from 'connectors/discoverItems/items.state'
 
+import { wrapper } from 'store'
 /**
  * Server Side State Hydration
  * This is where we are defining initial state for this page.
@@ -13,69 +14,86 @@ import { hydrateItems } from 'connectors/discoverItems/items.state'
  * until they are resolved, which is fine if we need the data for
  * SEO/Crawlers
   --------------------------------------------------------------- */
-Topic.getInitialProps = async ({ store, query, res }) => {
-  const { dispatch } = store
-  const { slug } = query
-
-  if (slug === 'null') return { namespacesRequired: ['common'] }
-
-  // Hydrating initial state with an async request. This will block the
-  // page from loading. Do this for SEO/crawler purposes
+export const getStaticPaths = async () => {
   const topicsByName = await fetchTopicList()
-
-  // Invalid Topic
-  const searchDefault = { topic: slug, page_type: 'search' }
-  const activeTopic = topicsByName[slug] || searchDefault
-
-  // Get topic info
-  const { topic, page_type } = activeTopic
-  const isCollection = page_type === 'editorial_collection'
-  const allowedSearch = ['productivity', 'finance']
-  const validSearchTopic = allowedSearch.includes(topic.toLowerCase())
-
-  // We are not fully supporting search at the moment
-  if (page_type === 'search' && !validSearchTopic) {
-    res.append('X-Robots-Tag', 'none')
-    res.statusCode = 404
-    return { statusCode: 404 }
-  }
-
-  // Get topic items from the correct endpoint
-  const response =
-    page_type === 'search'
-      ? await fetchSearchData(topic)
-      : await fetchTopicData(topic, isCollection)
-
-  if (!response) {
-    res.statusCode = 500
-    return { statusCode: 500 }
-  }
-
-  const {
-    searchItems,
-    curatedItems,
-    algorithmicItems,
-    searchItemsById,
-    curatedItemsById,
-    algorithmicItemsById
-  } = response
-
-  // Hydrate the items array
-  dispatch(
-    hydrateItems({
-      ...searchItemsById,
-      ...curatedItemsById,
-      ...algorithmicItemsById
-    })
+  const paths = Object.values(topicsByName).map(
+    (data) => `/discover/${data.topic_slug}`
   )
-
-  // Hydrate the topic list state
-  dispatch(hydrateTopicList({ activeTopic: slug, topicsByName }))
-
-  // Hydrate the topic page item arrays
-  dispatch(hydrateTopic({ searchItems, curatedItems, algorithmicItems }))
-
-  return { namespacesRequired: ['common'] }
+  return { paths, fallback: false }
 }
+
+export const getStaticProps = wrapper.getStaticProps(
+  async ({ store, params }) => {
+    const { slug } = params
+
+    if (slug === 'null') return { props: { namespacesRequired: ['common'] } }
+
+    const { dispatch } = store
+
+    // Hydrating initial state with an async request. This will block the
+    // page from loading. Do this for SEO/crawler purposes
+    const topicsByName = await fetchTopicList()
+
+    // Invalid Topic
+    const searchDefault = { topic: slug, page_type: 'search' }
+    const activeTopic = topicsByName[slug] || searchDefault
+
+    // Get topic info
+    const { topic, page_type } = activeTopic
+    const isCollection = page_type === 'editorial_collection'
+    const allowedSearch = ['productivity', 'finance']
+    const validSearchTopic = allowedSearch.includes(topic.toLowerCase())
+
+    // We are not fully supporting search at the moment
+    if (page_type === 'search' && !validSearchTopic) {
+      res.append('X-Robots-Tag', 'none')
+      res.statusCode = 404
+      return { statusCode: 404 }
+    }
+
+    // Get topic items from the correct endpoint
+    const response =
+      page_type === 'search'
+        ? await fetchSearchData(topic)
+        : await fetchTopicData(topic, isCollection)
+
+    if (!response) {
+      res.statusCode = 500
+      return { statusCode: 500 }
+    }
+
+    const {
+      searchItems = [],
+      curatedItems = [],
+      algorithmicItems = [],
+      searchItemsById = {},
+      curatedItemsById = {},
+      algorithmicItemsById = {}
+    } = response
+
+    // Hydrate the items array
+    dispatch(
+      hydrateItems({
+        ...searchItemsById,
+        ...curatedItemsById,
+        ...algorithmicItemsById
+      })
+    )
+
+    // Hydrate the topic list state
+    dispatch(hydrateTopicList({ activeTopic: slug, topicsByName }))
+
+    // Hydrate the topic page item arrays
+    dispatch(
+      hydrateTopic({
+        topic: slug,
+        data: { searchItems, curatedItems, algorithmicItems }
+      })
+    )
+
+    // Revalidate means this can be regenerated once every X seconds
+    return { props: { namespacesRequired: ['common'] }, revalidate: 60 }
+  }
+)
 
 export default Topic
