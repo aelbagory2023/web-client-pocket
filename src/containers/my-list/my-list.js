c10c848307aff8aed553eae36562aef9c5a6c661
Joel Kelly <joel@getpocket.com>
2020-10-20 14:48:26 -0700

    feat(my list): utilizing filters/reconcilers/etc

--- src/containers/my-list/my-list.js
+++ src/containers/my-list/my-list.js
@@ -1,9 +1,12 @@
+// @refresh reset
 // Vendor
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useRouter } from 'next/router'
 
 import Layout from 'layouts/with-sidebar'
 import { getMylistData } from './my-list.state'
+import { updateMyListData } from './my-list.state'
 
 import { MyListHeader } from 'components/headers/my-list-header'
 
@@ -15,30 +18,52 @@ export default function Collection(props) {
 
   // useEffect(trackPageView, [])
   const dispatch = useDispatch()
+  const router = useRouter()
+
   const section = filter ? subset + filter : subset
   const items = useSelector((state) => state.myList[section])
   const offset = useSelector((state) => state.myList[`${section}Offset`])
   const total = useSelector((state) => state.myList[`${section}Total`])
+  const since = useSelector((state) => state.myList[`${section}Since`])
+
+  const handleFocus = () => {
+    if (!since) return
+    dispatch(updateMyListData(since, subset, filter))
+  }
+
+  useEffect(() => {
+    // Adding new event listeners
+    window.addEventListener('focus', handleFocus)
+
+    // Remove event listeners when it is unmounted
+    return () => {
+      window.removeEventListener('focus', handleFocus)
+    }
+  }, [handleFocus])
 
   // Check if we have requested list items
   useEffect(() => {
-    // We grab a small set of items for initial load quickly
-    if (!items?.length) {
+    dispatch(getMylistData(18, 0, subset, filter))
+  }, [])
+
+  useEffect(() => {
+    // Check if we are navigating to a route with items in place
+    if (!since) {
       dispatch(getMylistData(18, 0, subset, filter))
       return
     }
 
-    // Subsequent runs we make a check to see if we need to prime the list
-    const gatherMoreItems = offset === 18 && offset <= total
-    if (gatherMoreItems) dispatch(getMylistData(54, offset, subset, filter))
-
-    return
-  }, [items])
+    // If items are already in place, we want to know if anything has changed
+    // since the last time we fetched the list (operations in other pages or apps)
+    dispatch(updateMyListData(since, subset, filter))
+  }, [router.pathname, dispatch])
 
   // Explicit request to load more items
   const loadMore = () => dispatch(getMylistData(150, offset, subset, filter))
 
+  // TODO: Adjust this to use app state
   const type = 'grid'
+
   return (
     <Layout title={metaData.title} metaData={metaData}>
       <SideNav subset={subset} />
