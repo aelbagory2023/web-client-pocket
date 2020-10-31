48671a7cc71bdf0199d4abdc5f58a824bd360402
Joel Kelly <joel@getpocket.com>
2020-10-30 18:50:20 -0700

    refactor(reader): remove initial props

--- src/pages/read/[slug].js
+++ src/pages/read/[slug].js
@@ -1,13 +1,3 @@
 import Reader from 'containers/read/read'
 
-Reader.getInitialProps = async ({ query }) => {
-  const { slug } = query
-
-  return {
-    namespacesRequired: ['common'],
-    appRootSelector: '#__next',
-    itemId: slug
-  }
-}
-
 export default Reader
