10aa8cf79880a697445d1c14ac2060eac441827b
Joel Kelly <joel@getpocket.com>
2020-12-02 09:47:39 -0800

    feat(search): adding search specific header

---
+++ src/components/headers/search-page-header.js
@@ -0,0 +1,48 @@
+import { css } from 'linaria'
+import { breakpointSmallHandset } from '@pocket/web-ui'
+import { breakpointLargeHandset } from '@pocket/web-ui'
+import { breakpointTinyTablet } from '@pocket/web-ui'
+import { breakpointLargeTablet } from '@pocket/web-ui'
+import { FilterMenu } from 'components/list-filter-menu/list-filter-menu'
+
+const searchPageHeaderStyle = css`
+  margin-bottom: var(--spacing100);
+  font-family: 'Graphik Web';
+
+  h1 {
+    display: inline-block;
+    font-family: 'Graphik Web';
+    font-style: normal;
+    font-weight: 500;
+    font-size: var(--fontSize150);
+    line-height: 1.2;
+    letter-spacing: -0.005em;
+    margin-bottom: var(--spacing150);
+
+    ${breakpointLargeTablet} {
+      font-size: var(--fontSize200);
+    }
+
+    ${breakpointTinyTablet} {
+      margin-bottom: var(--spacing100);
+    }
+
+    ${breakpointLargeHandset} {
+      font-size: var(--fontSize150);
+    }
+
+    ${breakpointSmallHandset} {
+      font-size: var(--fontSize125);
+      margin-bottom: var(--spacing100);
+    }
+  }
+`
+
+export const SearchPageHeader = ({ subset, filter, title }) => {
+  return subset ? (
+    <header className={searchPageHeaderStyle}>
+      <h1 className="pageTitle">Search: {title}</h1>
+      <FilterMenu subset={subset} filter={filter} />
+    </header>
+  ) : null
+}
