67dfdf05ce39cb99b496eec1dd7005c6f6e3905d
Joel Kelly <joel@getpocket.com>
2020-12-02 09:48:02 -0800

    feat(search): adding router push to search topic

--- src/connectors/global-nav/global-nav-search.js
+++ src/connectors/global-nav/global-nav-search.js
@@ -1,9 +1,11 @@
 import GlobalNavSearch from 'components/global-nav/tools/search/global-nav-search'
+import { useRouter } from 'next/router'
+import escape from 'validator/lib/escape'
 
 function GlobalNavSearchConnected({ onClose }) {
+  const router = useRouter()
   const onSubmit = (searchTerm) => {
-    console.log('Submit Search:', searchTerm)
-    onClose()
+    router.push(`/my-list/search/?query=${escape(searchTerm)}`)
   }
   return <GlobalNavSearch onClose={onClose} onSubmit={onSubmit} />
 }
