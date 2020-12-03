67e49f29257a65460caeb5453305e704a5488967
Joel Kelly <joel@getpocket.com>
2020-12-03 12:26:35 -0800

    feat(search): adding search specific api
    easier to debug when not overloading

--- src/common/api/my-list.js
+++ src/common/api/my-list.js
@@ -24,6 +24,27 @@ export function getMyList(data) {
   })
 }
 
+export function searchMyList(data) {
+  return request({
+    path: 'v3/get',
+    auth: true,
+    params: {
+      tags: 1, //	int	1 to include Tags in Items
+      positions: 1, //	int	1 to include Positions in Items
+      authors: 1, //	int	1 to include Authors in Items
+      images: 1, //	int	1 to include Images in Items
+      videos: 1, //	int	1 to include Videos in Items
+      itemTopics: 1, //	int	1 to include Topics in Items
+      meta: 1, //	int	1 to include ItemMeta in Items
+      posts: 1, //	int	1 to include Posts in Items
+      annotations: 1, //	int	1 to include Annotations in Items
+      total: 1, // include total number of items
+      attribution: 2, //	int	2 to include attributionTypes in the response as well as including ExtendAttribution in Items
+      ...data
+    }
+  })
+}
+
 export function fetchMyList(params) {
   return request({
     path: 'v3/fetch',
