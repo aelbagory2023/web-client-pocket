135a12c070c6dd1f7f28eaa5ab52565114044692
Joel Kelly <joel@getpocket.com>
2020-12-04 11:18:17 -0800

    feat(animation): adding a fade wrapper

---
+++ src/common/utilities/animation/fade.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import { css, cx } from 'linaria'
+
+const fadeStyle = css`
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  @keyframes fadeOut {
+    0% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
+  &.fadeIn {
+    animation: fadeIn 1s;
+  }
+
+  &.fadeOut {
+    animation: fadeOut 1s;
+  }
+`
+
+export const Fade = ({ show, remove, children }) => {
+  const [render, setRender] = useState(show)
+
+  useEffect(() => {
+    if (show) setRender(true)
+  }, [show])
+
+  const onAnimationEnd = () => {
+    if (!show) {
+      setRender(false)
+      remove()
+    }
+  }
+
+  const fadeClass = cx(fadeStyle, show && 'fadeIn', !show && 'fadeOut')
+
+  return (
+    render && (
+      <div className={fadeClass} onAnimationEnd={onAnimationEnd}>
+        {children}
+      </div>
+    )
+  )
+}
