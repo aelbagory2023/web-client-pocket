baa3df93d0c514ae39f19a692f4eae5aee03cd40
Joel Kelly <joel@getpocket.com>
2020-12-10 12:33:30 -0800

    refactor(stories): use native menu for theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,21 +1,42 @@
 import '@pocket/web-ui/lib/pocket-web-ui.css'
+import { withNextRouter } from 'storybook-addon-next-router'
 
 import React from 'react'
 import { Provider } from 'react-redux'
 import { initializeStore } from 'store'
-import { ColorModePicker } from '@pocket/web-ui'
 
 const store = initializeStore()
 
 export const parameters = {
+  backgrounds: { disable: true },
   actions: { argTypesRegex: '^on[A-Z].*' }
 }
 
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'photo',
+      // array of plain string values or MenuItem shape (see below)
+      items: ['light', 'dark', 'sepia']
+    }
+  }
+}
+
 export const decorators = [
-  (Story) => (
-    <Provider store={store}>
-      <ColorModePicker floating />
-      <Story />
-    </Provider>
-  )
+  (Story, context) => {
+    document.body.classList.remove('colormode-light')
+    document.body.classList.remove('colormode-dark')
+    document.body.classList.remove('colormode-sepia')
+    document.body.classList.add(`colormode-${context.globals.theme}`)
+
+    return (
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    )
+  },
+  withNextRouter
 ]
