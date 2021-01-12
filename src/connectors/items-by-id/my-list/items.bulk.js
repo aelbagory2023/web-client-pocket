68a7253386313301572b7631fccf4ddf9bbca97d
Anthony Liddle <anthonyliddle@gmail.com>
2021-01-12 12:59:53 -0800

    fix(bulk): update batch status on favorite/unfavorite

--- src/connectors/items-by-id/my-list/items.bulk.js
+++ src/connectors/items-by-id/my-list/items.bulk.js
@@ -12,6 +12,9 @@ import { ITEMS_BULK_BATCH_FAILURE } from 'actions'
 import { ITEMS_BULK_BATCH_COMPLETE } from 'actions'
 import { ITEMS_ARCHIVE_CANCEL } from 'actions'
 
+import { ITEMS_FAVORITE_BATCH } from 'actions'
+import { ITEMS_UNFAVORITE_BATCH } from 'actions'
+
 /** ACTIONS
  --------------------------------------------------------------- */
 export const itemsBulkSelectAction = (id, shift) => ({ type: ITEMS_BULK_SELECT, id, shift }) //prettier-ignore
@@ -74,6 +77,13 @@ export const itemBulkReducers = (state = initialState, action) => {
     case ITEMS_BULK_BATCH_FAILURE: {
       const { batchCount } = action
       return { ...state, batchCount }
+    }
+
+    case ITEMS_FAVORITE_BATCH: {
+      return { ...state, batchFavorite: 'unfavorite' }
+    }
+    case ITEMS_UNFAVORITE_BATCH: {
+      return { ...state, batchFavorite: 'favorite' }
     }
 
     default:
