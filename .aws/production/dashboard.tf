53f35ca73a0eaaea946f4105214d6d57f190f71b
Joel Kelly <joel@getpocket.com>
2021-01-26 15:11:51 -0800

    fix(terraform): adjusting variables

--- .aws/production/dashboard.tf
+++ .aws/production/dashboard.tf
@@ -1,10 +1,10 @@
 data "template_file" "dashboard" {
   template = file("files/dashboard.json")
   vars     = {
-    ecs_cluster_name            = aws_ecs_cluster.ecs_cluster.name
+    ecs_cluster_name            = aws_ecs_cluster.ecs-cluster.name
     ecs_service_name            = aws_ecs_service.public.name
-    alb_arn_suffix              = aws_alb.alb.arn_suffix
-    target_group_arn_suffix     = aws_alb_target_group.alb_target.arn_suffix
+    alb_arn_suffix              = data.aws_alb.web_client.arn_suffix
+    target_group_arn_suffix     = aws_alb_target_group.public.arn_suffix
     cpu_scale_out               = local.repo_context.cpu_scale_out
     cpu_scale_in                = local.repo_context.cpu_scale_in
     log_group_prefix            = "/ecs/${local.repo_context.name}/${local.environment}"
