61316a6f3d6b8d8e0b096e9b26c88851aa047819
Anthony Liddle <anthonyliddle@gmail.com>
2021-01-27 08:46:55 -0800

    fix(messages): add translation function to imports

--- src/components/messages/messages-empty.js
+++ src/components/messages/messages-empty.js
@@ -2,26 +2,30 @@ import { css } from 'linaria'
 import { fontSansSerif } from '@pocket/web-ui'
 import { MessagesHeader } from 'components/headers/messages-header'
 import classNames from 'classnames'
-import { Trans } from 'common/setup/i18n'
+import { Trans, useTranslation } from 'common/setup/i18n'
 
 const emptyWrapper = css`
   font-family: ${fontSansSerif};
 `
 
-export const MessageEmpty = () => (
-  <main className={classNames('main', emptyWrapper)}>
-    <MessagesHeader title={t('messages:inbox-is-empty', 'Your Inbox is Empty')} />
-    <p>
-      <Trans i18nKey="messages:when-someone-shares">
-        When someone shares items with you using Send to Friend, they will
-        appear here.
-      </Trans>
-    </p>
-    <p>
-      <Trans i18nKey="messages:you-can-send-to-friend">
-        You can also use Send to Friend to share items directly to a friend’s
-        Pocket. Look for Send to Friend in the Share menu.
-      </Trans>
-    </p>
-  </main>
-)
+export const MessageEmpty = () => {
+  const { t } = useTranslation()
+
+  return (
+    <main className={classNames('main', emptyWrapper)}>
+      <MessagesHeader title={t('messages:inbox-is-empty', 'Your Inbox is Empty')} />
+      <p>
+        <Trans i18nKey="messages:when-someone-shares">
+          When someone shares items with you using Send to Friend, they will
+          appear here.
+        </Trans>
+      </p>
+      <p>
+        <Trans i18nKey="messages:you-can-send-to-friend">
+          You can also use Send to Friend to share items directly to a friend’s
+          Pocket. Look for Send to Friend in the Share menu.
+        </Trans>
+      </p>
+    </main>
+  )
+}
