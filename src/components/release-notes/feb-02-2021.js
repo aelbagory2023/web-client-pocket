8b363db67de93518489ecff83674cd93d4614528
Anthony Liddle <anthonyliddle@gmail.com>
2021-01-29 15:26:03 -0800

    fix(whats-new): updating messaging

--- src/components/release-notes/feb-02-2021.js
+++ src/components/release-notes/feb-02-2021.js
@@ -6,6 +6,15 @@ export default function ReleaseNotes() {
       <Trans i18nKey="release:february-02-2021">
         <h5>February 2, 2021</h5>
 
+        <h6>🎉 What’s New</h6>
+        <p>
+          Detailed list view (with tags!) - we’ve added a third display option
+          for the list to give more context  to the items you’ve saved. Switch
+          to detailed list view in the account settings menu to see tags and
+          preview text for your articles.
+        </p>
+
+        <h6>🛠️ Upkeep</h6>
         <p>
           For improved performance and stability, we’ve removed Send to Friend
           from the Share menu. To share outside of Pocket, click on the share
