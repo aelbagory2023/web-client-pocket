a3be9709c777d3f42687def37eb03aea841cece8
Joel Kelly <joel@getpocket.com>
2021-01-29 21:18:04 -0800

    fix(color mode): avoid flash of default theme

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,37 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { COLOR_MODE_PREFIX, CACHE_KEY_COLOR_MODE } from 'common/constants'
 
 class ClientDocument extends Document {
   render() {
     return (
-      <Html className="colormode-light">
+      <Html>
         <Head />
         <body>
+          {/*
+          This all sets us up for color themes without having a flash of
+          light if the user has chosen another color mode as the default
+          */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function () {
+
+              function setColorMode(colorMode) {
+                const htmlTag = document && document.documentElement
+                htmlTag.classList.toggle('${COLOR_MODE_PREFIX}-light', (!colorMode || colorMode === 'light'))
+                htmlTag.classList.toggle('${COLOR_MODE_PREFIX}-dark', colorMode === 'dark')
+                htmlTag.classList.toggle('${COLOR_MODE_PREFIX}-sepia', colorMode === 'sepia')
+              }
+
+              let preferredColorMode;
+              try {
+                preferredColorMode = localStorage.getItem('${CACHE_KEY_COLOR_MODE}');
+              } catch (err) {}
+
+              setColorMode(preferredColorMode);
+            })();
+            `
+            }}
+          />
           <Main />
           <NextScript />
         </body>
