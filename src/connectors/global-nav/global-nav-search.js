9461fdf80f814da6084ed246204ac655f1881d08
Joel Kelly <joel@getpocket.com>
2021-02-04 10:58:56 -0800

    feat(recent-search): pass in premium/recent

--- src/connectors/global-nav/global-nav-search.js
+++ src/connectors/global-nav/global-nav-search.js
@@ -1,18 +1,28 @@
 import GlobalNavSearch from 'components/global-nav/tools/search/global-nav-search'
 import { useRouter } from 'next/router'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { sendSearchEvent } from './global-nav.analytics'
 import escape from 'validator/lib/escape'
 
 function GlobalNavSearchConnected({ onClose }) {
   const router = useRouter()
   const dispatch = useDispatch()
+  const isPremium = useSelector((state) => parseInt(state?.user?.premium_status, 10) === 1 || false) //prettier-ignore
+  const recentSearches = useSelector((state) => state.userSearch.recent)
 
   const onSubmit = (searchTerm) => {
     dispatch(sendSearchEvent('global-nav.search'))
     router.push(`/my-list/search/?query=${escape(searchTerm)}`)
   }
-  return <GlobalNavSearch onClose={onClose} onSubmit={onSubmit} />
+
+  return (
+    <GlobalNavSearch
+      recentSearches={recentSearches}
+      onClose={onClose}
+      onSubmit={onSubmit}
+      isPremium={isPremium}
+    />
+  )
 }
 
 export default GlobalNavSearchConnected
