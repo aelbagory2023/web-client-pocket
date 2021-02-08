8a4b0626cf1dafa8530f1a5abc1cc40afdf6d18f
Joel Kelly <joel@getpocket.com>
2021-02-08 10:45:01 -0800

    refactor(search): make index static

--- src/pages/my-list/search/index.js
+++ src/pages/my-list/search/index.js
@@ -1,3 +1,11 @@
 import SearchPage from 'containers/my-list/search-page/search-page'
 
+export async function getStaticProps() {
+  return {
+    props: {
+      namespacesRequired: ['common']
+    }
+  }
+}
+
 export default SearchPage
