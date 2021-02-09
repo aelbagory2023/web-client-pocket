4b4ac8d68dd33bf738073d2911100316bb80d2e8
Joel Kelly <joel@getpocket.com>
2021-02-08 16:46:38 -0800

    refactor(sentry): re-add tracing

--- src/common/setup/sentry.js
+++ src/common/setup/sentry.js
@@ -1,8 +1,15 @@
+import { Integrations } from '@sentry/tracing'
 const isDev = process.env.NODE_ENV !== 'production'
 
 export const sentrySettings = {
   dsn:
     'https://cc3aea2dd5ca4aefb5a18c671a229237@o28549.ingest.sentry.io/5436250',
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ['localhost.web.getpocket.com/', 'getpocket.com']
+    })
+  ],
   release: process.env.BUILD_ID,
+  tracesSampleRate: 0,
   environment: isDev ? 'Development' : 'Production'
 }
