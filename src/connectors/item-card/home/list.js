9841e21668a2e8a09812a102cd3befde09f401c1
Joel Kelly <joel@getpocket.com>
2021-02-20 07:51:14 -0800

    refactor(home): remove button

--- src/connectors/item-card/home/list.js
+++ src/connectors/item-card/home/list.js
@@ -3,15 +3,16 @@ import { useDispatch, useSelector } from 'react-redux'
 import { css } from 'linaria'
 import { ItemCard } from './card'
 import { cardGrid } from 'components/items-layout/virtualized-list'
-import { Button } from '@pocket/web-ui'
 import classnames from 'classnames'
 
 const topicRowStyles = css`
-  margin-bottom: 3.5rem;
+  margin-bottom: 1.5rem;
 `
 
 const cardRowStyles = css`
   margin-bottom: var(--spacing100);
+  padding-bottom: 2rem;
+  border-bottom: var(--dividerStyle);
 `
 
 const topicSubheadings = {
@@ -19,14 +20,14 @@ const topicSubheadings = {
   career: false,
   education: false,
   entertainment: false,
-  food: "Stories to fuel your body, curated by our editors.",
+  food: 'Stories to fuel your body, curated by our editors.',
   gaming: false,
   health: false,
   parenting: false,
   'personal-finance': false,
   politics: false,
   science: false,
-  'self-improvement': "Stories to level up your life, curated by our editors.",
+  'self-improvement': 'Stories to level up your life, curated by our editors.',
   sports: false,
   technology: false,
   travel: false
@@ -46,23 +47,26 @@ export const HomeTopicsList = ({
     <div className={topicRowStyles}>
       <HomeSectionHeader
         sectionTitle={display_name}
-        sectionDescription={topicSubheadings[topic] || "We need something better here, curated by our editors."}
+        topicSlug={topic_slug}
+        sectionDescription={
+          topicSubheadings[topic] ||
+          'We need something better here, curated by our editors.'
+        }
       />
       <section className={classnames(cardGrid, cardRowStyles)}>
-        {topicItems ? topicItems.map((id, index) => (
-          <ItemCard
-            key={id}
-            id={id}
-            topic={topic}
-            saveAction={saveAction}
-            unSaveAction={unSaveAction}
-            position={index}
-          />
-        )) : null }
+        {topicItems
+          ? topicItems.map((id, index) => (
+              <ItemCard
+                key={id}
+                id={id}
+                topic={topic}
+                saveAction={saveAction}
+                unSaveAction={unSaveAction}
+                position={index}
+              />
+            ))
+          : null}
       </section>
-      <Button href={`/discover/${topic_slug}`} target="_blank">
-        Discover {display_name}
-      </Button>
     </div>
   )
 }
