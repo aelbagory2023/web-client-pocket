2e010e41af44bb56f9e5f779ff786aa1fd8df94e
Anthony Liddle <anthonyliddle@gmail.com>
2021-02-22 16:28:33 -0800

    fix(snowplow): update resolved id to item id

--- src/connectors/snowplow/entities/content.js
+++ src/connectors/snowplow/entities/content.js
@@ -2,12 +2,9 @@ import { getSchemaUri } from 'common/api/snowplow-analytics'
 
 export const CONTENT_SCHEMA_URL = getSchemaUri('content')
 
-const createContentEntity = (url, resolvedId, itemId) => {
+const createContentEntity = (url, itemId) => {
   const data = { url }
-  // possible resolvedId is 0, return itemId in that instance
-  if (resolvedId || itemId) {
-    data.item_id = parseInt(resolvedId, 10) || parseInt(itemId, 10)
-  }
+  if (itemId) data.item_id = parseInt(itemId, 10)
 
   return {
     schema: CONTENT_SCHEMA_URL,
