312c4214e00e28fe1737beb6216d302f0cebb6b1
Anthony Liddle <anthonyliddle@gmail.com>
2021-02-25 16:25:39 -0800

    fix(snowplow): adding the version as an environment variable

--- src/connectors/snowplow/entities/api-user.js
+++ src/connectors/snowplow/entities/api-user.js
@@ -1,13 +1,15 @@
 import { API_USER_ID } from 'common/constants'
 import { getSchemaUri } from 'common/api/snowplow-analytics'
 
+const RELEASE_VERSION = process.env.RELEASE_VERSION || '0.0.0'
+
 const API_USER_SCHEMA_URL = getSchemaUri('api_user', '1-0-1')
 
 export const createApiUserEntity = (appUserId) => ({
   schema: API_USER_SCHEMA_URL,
   data: {
     api_id: appUserId,
-    client_version: '1.0.0'
+    client_version: RELEASE_VERSION
   }
 })
 
