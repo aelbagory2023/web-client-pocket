a76bcf36abaf145fcc75b631cc6b617e7aec016c
Joel Kelly <joel@getpocket.com>
2021-03-05 10:27:53 -0800

    refacrtor(collections api): pare down data set

--- src/pages/api/getCollections.js
+++ src/pages/api/getCollections.js
@@ -2,9 +2,9 @@ const collectionData = require('./_data/collections.json')
 
 export default function handler(req, res) {
   const { count = 3 } = req?.query
-  const { collections } = collectionData
-  
-  const randomSet = collections.sort(() => 0.5 - Math.random()).slice(0, count)
+  const randomSet = collectionData
+    .sort(() => 0.5 - Math.random())
+    .slice(0, count)
 
   res.status(200).json(JSON.stringify(randomSet))
 }
