de22948207b72bff35d1bb7009e41bfc1546f9a8
Joel Kelly <joel@getpocket.com>
2021-03-16 10:44:25 -0700

    refactor(request): remove oauth

--- src/common/utilities/request/request.js
+++ src/common/utilities/request/request.js
@@ -1,8 +1,5 @@
 import queryString from 'query-string'
-import { CONSUMER_KEY, API_URL, OAUTH_URL } from 'common/constants'
-import { parseCookies } from 'nookies'
-import crypto from 'crypto'
-import { localStore } from 'common/utilities/browser-storage/browser-storage'
+import { CONSUMER_KEY, API_URL } from 'common/constants'
 
 /**
  * @param {Object} requestInfo Information about the request
@@ -19,14 +16,8 @@ export const request = ({
   auth,
   ssr
 }) => {
-  const useOAuth = localStore.getItem('useOAuth') === 'true'
-  // !! THIS TEMPORARY 12/14/2020
-  // This is all meant to allow for feature branch auth via oAuth while keeping
-  // cookie based auth on local dev and production
-  // !! THIS SHOULD BE REMOVED IN 2077 When auth is fixed
-  const oAuthParameters = useOAuth ? generateOAuthParameters(auth) : {}
-  const credentials = useOAuth ? 'omit' : 'include'
-  const apiToUse = useOAuth ? OAUTH_URL : api_url
+  const credentials = 'include'
+  const apiToUse = api_url
 
   const corsParameters = {}
   if (!ssr) corsParameters.enable_cors = 1
@@ -34,8 +25,7 @@ export const request = ({
   const queryParams = queryString.stringify({
     ...params,
     ...corsParameters,
-    consumer_key: CONSUMER_KEY,
-    ...oAuthParameters
+    consumer_key: CONSUMER_KEY
   })
 
   const endpoint = `${apiToUse}/${path}?${queryParams}`
@@ -75,36 +65,4 @@ function handleErrors(response, auth) {
 
   // If all else we re returning the response to be parsed
   return response
-}
-
-function generateOAuthParameters(auth) {
-  if (!auth || !document) return {}
-  const cookies = parseCookies()
-  const { pkt_access_token } = cookies
-
-  if (!pkt_access_token) return {}
-
-  const salt = 'It will take more than a doctor to prescribe a remedy'
-  const oauth_timestamp = Date.now()
-  const oauth_nonce = randomAlphaNumeric(16)
-  const sig = oauth_timestamp + oauth_nonce + pkt_access_token + salt
-  const sig_hash = crypto.createHash('md5').update(sig).digest('hex')
-
-  return {
-    access_token: pkt_access_token,
-    oauth_timestamp,
-    oauth_nonce,
-    sig_hash
-  }
-}
-
-function randomAlphaNumeric(length) {
-  var result = ''
-  var characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  var charactersLength = characters.length
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength))
-  }
-  return result
 }
