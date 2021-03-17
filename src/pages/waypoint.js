0eae76fbdf2f945fc2781deb46b12ee1ff73a277
Joel Kelly <joel@getpocket.com>
2021-03-17 16:44:48 -0700

    fix(waypoint): allow proper redirection

--- src/pages/waypoint.js
+++ src/pages/waypoint.js
@@ -1,14 +1,34 @@
-import { useEffect } from 'react'
+import { getUserInfo } from 'common/api/user'
+import { fetchUnleashData } from 'connectors/feature-flags/feature-flags.state'
 
-export default function Waypoint({ serverCookies }) {
-  useEffect(() => {
-    console.log(serverCookies)
-  }, [serverCookies])
-
-  return <div></div>
-}
+export default function Waypoint() {}
 
 export async function getServerSideProps({ res, req }) {
-  const serverCookies = req.cookies
-  return { props: { serverCookies } }
+  const { sess_guid } = req.cookies
+  const response = await getUserInfo(true)
+  // Not logged in, or something else went awry?
+  // NOTE: this will redirect to my list 100% of the time on localhost
+  const { user_id } = response?.user_id || {}
+  if (!user_id) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/my-list'
+      }
+    }
+  }
+
+  // This sets up the home test. If a user is assigned to the test we send them
+  // from waypoint to home.  Otherwise they get sent to my-list
+  const features = await fetchUnleashData(user_id, sess_guid)
+  const destination = features['temp.web.client.home.new_user'].assigned
+    ? '/home'
+    : '/my-list'
+
+  return {
+    redirect: {
+      permanent: false,
+      destination
+    }
+  }
 }
