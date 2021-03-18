55d5d144f0c87fd5f02061735a8e20e30a68ff08
Joel Kelly <joel@getpocket.com>
2021-03-18 11:20:21 -0700

    fix(waypoint): add some sentry logging

--- src/pages/waypoint.js
+++ src/pages/waypoint.js
@@ -1,14 +1,33 @@
+import * as Sentry from '@sentry/node'
 import { getUserInfo } from 'common/api/user'
 import { fetchUnleashData } from 'connectors/feature-flags/feature-flags.state'
 
 export default function Waypoint() {}
 
-export async function getServerSideProps({ res, req }) {
+export async function getServerSideProps({ req }) {
+  Sentry.withScope((scope) => {
+    scope.setTag('ssr', 'WayPoint')
+    Object.keys(req.cookies).forEach((key) => {
+      scope.setExtra(key, req.cookies[key])
+    })
+    scope.setLevel('info')
+    Sentry.captureMessage('Waypoint: Request Cookies')
+  })
+
   const { sess_guid } = req.cookies
   const response = await getUserInfo(true, req?.headers?.cookie)
   // Not logged in, or something else went awry?
   // NOTE: this will redirect to my list 100% of the time on localhost
   const { user_id } = response?.user_id || {}
+
+  Sentry.withScope((scope) => {
+    scope.setTag('ssr', 'WayPoint')
+    scope.setExtra('user', response)
+    scope.setExtra('sessGuid', sess_guid)
+    scope.setLevel('info')
+    Sentry.captureMessage('Waypoint: User Response')
+  })
+
   if (!user_id) {
     return {
       redirect: {
@@ -21,6 +40,13 @@ export async function getServerSideProps({ res, req }) {
   // This sets up the home test. If a user is assigned to the test we send them
   // from waypoint to home.  Otherwise they get sent to my-list
   const features = await fetchUnleashData(user_id, sess_guid)
+  Sentry.withScope((scope) => {
+    scope.setTag('ssr', 'WayPoint')
+    scope.setExtra('features', features)
+    scope.setLevel('info')
+    Sentry.captureMessage('Waypoint: Feature Response')
+  })
+
   const destination = features['temp.web.client.home.new_user'].assigned
     ? '/home'
     : '/my-list'
