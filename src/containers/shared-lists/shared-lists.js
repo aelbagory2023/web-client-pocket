0c16aae52e0d8d9b0e0aa6c0c3294dbf219e9ca3
Joel Kelly <joel@getpocket.com>
2021-03-30 14:48:56 -0700

    refactor(shared-lists): adjusting response layout

--- src/containers/shared-lists/shared-lists.js
+++ src/containers/shared-lists/shared-lists.js
@@ -11,13 +11,14 @@ import { ENGAGEMENT_TYPE_GENERAL } from 'connectors/snowplow/events'
 import { UI_COMPONENT_BUTTON } from 'connectors/snowplow/entities'
 
 /* Analytics Event */
-export const sendEngagementEvent = (answer) => (trackEngagement(
-  ENGAGEMENT_TYPE_GENERAL,
-  UI_COMPONENT_BUTTON,
-  0, // position in list (zero since it's not in list)
-  'shared-lists.survey',
-  answer
-))
+export const sendEngagementEvent = (answer) =>
+  trackEngagement(
+    ENGAGEMENT_TYPE_GENERAL,
+    UI_COMPONENT_BUTTON,
+    0, // position in list (zero since it's not in list)
+    'shared-lists.survey',
+    answer
+  )
 
 const sharedListStyles = css`
   padding: 2rem 0;
@@ -27,12 +28,16 @@ const sharedListStyles = css`
     max-width: 50%;
     font-size: 0.85rem;
   }
-  .you {
+  .responseHeading {
+    font-family: 'Graphik Web';
     font-style: italic;
     font-weight: 600;
+    font-size: 1rem;
+    padding-bottom: 1rem;
   }
   .actions {
-    button, a {
+    button,
+    a {
       display: inline-block;
       margin-right: 1rem;
     }
@@ -41,14 +46,23 @@ const sharedListStyles = css`
 
 export default function SharedLists() {
   const dispatch = useDispatch()
-  const [answered, setAnswer] = useState(false)
+  const [answered, setAnswer] = useState('pending')
 
-  const onYesClick = () => dispatch(sendEngagementEvent('yes'))
-  const onMaybeClick = () => dispatch(sendEngagementEvent('maybe'))
+  const onYesClick = () => {
+    setAnswer('yes')
+    dispatch(sendEngagementEvent('yes'))
+  }
+
   const onNoClick = () => {
-    setAnswer(true)
+    setAnswer('no')
     dispatch(sendEngagementEvent('no'))
   }
+
+  const onSurveyClick = () => {
+    //open survey
+  }
+
+  const pending = answered === 'pending'
 
   return (
     <Layout>
@@ -59,50 +73,48 @@ export default function SharedLists() {
           sectionDescription="Share collections of content"
         />
         <div className={sharedListStyles}>
-          { answered ? (
-            <p>Thanks for your feedback!</p>
-          ) : (
+          <p>
+            Hello! We're thinking about offering people new ways to create and share lists of things
+            they've saved to Pocket.
+          </p>
+
+          <p>
+            Our first step is to get a sense of whether people might be interested in this feature
+            and why. Is this something that would interest you?
+          </p>
+
+          {pending ? (
+            <div className="actions">
+              <Button variant="secondary" target="_survey" onClick={onYesClick}>
+                Yes
+              </Button>
+
+              <Button variant="secondary" onClick={onNoClick}>
+                No
+              </Button>
+            </div>
+          ) : null}
+
+          {answered === 'no' ? <p>Thanks for your feedback!</p> : null}
+
+          {answered === 'yes' ? (
             <>
+              <div className="responseHeading">Awesome!</div>
               <p>
-                Hello! We’re thinking about offering people new ways to share lists
-                of things they've saved to Pocket.
+                We would love to hear more from you. If you have the time, please share your
+                thoughts in this survey. Thank you!
               </p>
-
-              <p>
-                Our first step is to get a sense of whether people might be interested
-                in this feature and why. If you could take a second and select your
-                interest below, that would be incredibly helpful. Thank you!
-              </p>
-
-              <p>
-                Would you be interested in sharing a list of articles with others?
-              </p>
-
               <div className="actions">
                 <Button
                   variant="secondary"
                   href="https://survey.alchemer.com/s3/6158777/Pocket-Shareable-Collections-2021"
                   target="_survey"
-                  onClick={onYesClick}>
-                  Yes
-                </Button>
-
-                <Button
-                  variant="secondary"
-                  href="https://survey.alchemer.com/s3/6158777/Pocket-Shareable-Collections-2021"
-                  target="_survey"
-                  onClick={onMaybeClick}>
-                  Maybe
-                </Button>
-
-                <Button
-                  variant="secondary"
-                  onClick={onNoClick}>
-                  No
+                  onClick={onSurveyClick}>
+                  Take a quick survey
                 </Button>
               </div>
             </>
-          )}
+          ) : null}
         </div>
       </main>
     </Layout>
