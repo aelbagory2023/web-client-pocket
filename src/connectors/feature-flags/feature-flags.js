0784ca5181f2d1bd1f96183b633c279fff34631a
Joel Kelly <joel@getpocket.com>
2021-04-05 11:55:19 -0700

    fix(unleash):  allow array of flags / add utility

--- src/connectors/feature-flags/feature-flags.js
+++ src/connectors/feature-flags/feature-flags.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-export function FeatureFlag({ children, flag, dev }) {
-  const showDevTools = process.env.SHOW_DEV === 'included'
-  const feature = useSelector((state) => state.features[flag])
-  const isFeatureEnabled = dev
-    ? showDevTools && feature?.assigned
-    : feature?.assigned
-  return isFeatureEnabled ? <>{children}</> : null
+export function FeatureFlag({ children, flag }) {
+  const featureState = useSelector((state) => state.features)
+  const isActive = featureFlagActive({ flag, featureState })
+  return isActive ? <>{children}</> : null
+}
+
+// Flag active selector
+export function featureFlagActive({ flag, featureState: features }) {
+  const flags = flag.isArray ? flag : [flag]
+
+  const activeFeatures = Object.keys(features)
+    .filter((feature) => flags.includes(features[feature].name))
+    .filter((feature) => features[feature].active)
+
+  return !!activeFeatures.length
 }
