5460ef96c3766c16b7c72bbc321bf09ab2b84276
Joel Kelly <joel@getpocket.com>
2021-04-23 09:19:53 -0700

    fix(item actions): consolidate for new card

---
+++ src/components/item-actions/base.js
@@ -0,0 +1,68 @@
+import { css } from 'linaria'
+
+export const itemActionStyle = css`
+  display: flex;
+  padding: var(--size100) 0 var(--size025);
+  justify-content: space-between;
+  .item-actions {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    align-content: center;
+    position: relative;
+    transform: translateX(-0.25rem);
+  }
+`
+
+export const menuItemStyle = css`
+  color: var(--color-textTertiary);
+  height: 24px;
+  background-color: transparent;
+  border: medium none;
+  padding: var(--size025);
+  margin-right: var(--size025);
+  box-sizing: content-box;
+  white-space: nowrap;
+  display: flex;
+  border-radius: var(--borderRadius);
+  transition: all 0.15s ease-out;
+
+  &.active {
+    color: var(--color-amber);
+    &:hover,
+    &:active,
+    &:focus {
+      color: var(--color-amber);
+    }
+  }
+
+  .icon {
+    height: 24px;
+    margin: 0;
+  }
+
+  .actionText {
+    display: inline-block;
+    margin-left: var(--size050);
+  }
+
+  svg {
+    fill: currentColor;
+    margin: 0;
+  }
+
+  &:hover,
+  &:active,
+  &:focus {
+    background-color: var(--color-canvas);
+    color: var(--color-textLinkHover);
+    cursor: pointer;
+  }
+
+  &:active,
+  &:focus {
+    transition: none;
+    color: var(--color-navCurrentTabText);
+    outline: 1px auto var(--color-navCurrentTab);
+  }
+`
---
+++ src/components/item-actions/save-to-pocket.js
@@ -0,0 +1,168 @@
+import { css } from 'linaria'
+import { usePopover, popoverBase } from 'components/popover/popover'
+import { LOGIN_URL, SIGNUP_URL } from 'common/constants'
+import classNames from 'classnames'
+import { buttonReset } from 'components/buttons/button-reset'
+import Link from 'next/link'
+
+import { SaveIcon } from '@pocket/web-ui'
+import { SaveFilledIcon } from '@pocket/web-ui'
+import { ReadingIcon } from '@pocket/web-ui'
+
+const saveContainer = css`
+  display: flex;
+  align-content: center;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: var(--fontSize150);
+  min-width: 3.913em;
+  color: var(--color-textSecondary);
+  cursor: pointer;
+  padding: 4px;
+  text-decoration: none;
+
+  .icon {
+    margin-top: 0;
+  }
+
+  svg {
+    transition: transform 200ms ease-out;
+    display: block;
+    margin-right: var(--size050);
+    height: 1em;
+  }
+
+  .actionCopy {
+    font-size: 1rem;
+    height: var(--size150);
+    line-height: var(--size150);
+    white-space: nowrap;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  &.read-now.saved {
+    svg {
+      color: var(--color-textSecondary);
+    }
+  }
+
+  &:hover,
+  &:focus,
+  &.saved,
+  &.read-now.saved:hover,
+  &.read-now.saved:focus {
+    text-decoration: none;
+    color: var(--color-textSecondary);
+    svg {
+      color: var(--color-actionBrand);
+    }
+  }
+
+  &:active,
+  &.saving {
+    svg {
+      transform: scale(0.95);
+      color: var(--color-actionBrandHover);
+    }
+  }
+`
+
+const popoverContainer = css`
+  ${popoverBase};
+  padding: 16px;
+  font-size: 16px;
+  line-height: 24px;
+  max-width: 180px;
+  .popoverLink {
+    font-weight: 500;
+    color: var(--color-actionPrimaryHover);
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`
+
+export const SavePopover = function ({ popoverRef, id }) {
+  const loginUrl = `${LOGIN_URL}?route=${global.location.href}`
+  const signupUrl = `${SIGNUP_URL}?route=${global.location.href}`
+  return (
+    //prettier-ignore
+    <div className={popoverContainer} ref={popoverRef} data-cy={`article-save-login-popup-${id}`}>
+      <a className="popoverLink" href={loginUrl}>Log in</a> or{' '}
+      <a className="popoverLink" href={signupUrl}>Sign up</a> to save this article.
+    </div>
+  )
+}
+
+/**
+ * Pocket logomark with click interaction to save a story to Pocket.
+ */
+export const SaveToPocket = function ({
+  url,
+  onOpen,
+  openExternal,
+  saveAction,
+  isAuthenticated,
+  saveStatus = 'unsaved',
+  allowRead = false,
+  id
+}) {
+  const saveCopy = {
+    unsaved: 'Save',
+    saving: 'Save',
+    saved: 'Saved'
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    saveAction(isAuthenticated)
+  }
+
+  // Popover Effect
+  const { popTrigger, popBody, shown } = usePopover({
+    placement: 'top-start',
+    modifiers: [
+      {
+        name: 'offset',
+        options: {
+          offset: [0, 0]
+        }
+      }
+    ]
+  })
+
+  const saveClasses = classNames(
+    buttonReset,
+    saveContainer,
+    saveStatus,
+    'card-actions',
+    allowRead && 'read-now'
+  )
+
+  return saveStatus === 'saved' && allowRead ? (
+    <Link href={url}>
+      <a onClick={onOpen} className={saveClasses} target={openExternal ? '_blank' : undefined}>
+        <ReadingIcon />
+        <span className="actionCopy">Read now</span>
+      </a>
+    </Link>
+  ) : (
+    <>
+      <button
+        className={saveClasses}
+        onClick={handleClick}
+        ref={popTrigger}
+        data-cy={`article-save-btn-${id}`}>
+        {saveStatus === 'saved' ? <SaveFilledIcon /> : <SaveIcon />}
+        <div className="actionCopy">{saveCopy[saveStatus]}</div>
+      </button>
+      {!isAuthenticated && shown ? <SavePopover popoverRef={popBody} id={id} /> : null}
+    </>
+  )
+}
