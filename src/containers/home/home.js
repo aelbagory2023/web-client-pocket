6e5086a1059f76e409f33a56c4d2736b94cd73cc
Joel Kelly <joel@getpocket.com>
2021-04-23 09:39:19 -0700

    fix(home): recs file structure

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -9,9 +9,9 @@ import { getTopicList } from 'connectors/topic-list/topic-list.state'
 import { getRecentSaves } from 'containers/home/home.state'
 import { getCollections } from 'containers/home/home.state'
 
-import { HomeGreeting } from './homeGreeting'
-import { HomeRecentRecsList } from 'containers/home/listRecentRecs'
-import { HomeRecentList } from 'containers/home/listRecents'
+import { HomeGreeting } from 'containers/home/homeGreeting'
+import { HomeRecentRecsList } from 'containers/home/list-recs'
+// import { HomeRecentList } from 'containers/home/listRecents'
 import { TopicSelector } from 'containers/home/topicSelector'
 import { HomeTopicsList } from 'containers/home/listTopics'
 import { HomeCollectionList } from 'containers/home/listCollection'
@@ -50,9 +50,7 @@ export default function Collection(props) {
       <SideNav subset="home" />
       {shouldRender ? (
         <main className="main">
-          { showLab ? (
-            <HomeGreeting />
-          ) : null}
+          { showLab ? <HomeGreeting /> : null }
 
           {/* <HomeRecentList /> */}
 
