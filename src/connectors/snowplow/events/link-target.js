16c0b4f7f82777da7be81340eac72bd38584de29
Joel Kelly <joel@getpocket.com>
2021-04-26 07:46:52 -0700

    fix(snowplow): allow for read tracking

--- src/connectors/snowplow/events/link-target.js
+++ src/connectors/snowplow/events/link-target.js
@@ -3,7 +3,9 @@ import { CONTENT_OPEN_DESTINATION_INTERNAL } from './content-open'
 import { CONTENT_OPEN_DESTINATION_EXTERNAL } from './content-open'
 
 export const getLinkOpenTarget = (link, isSyndicated = false) => {
-  const url = new URL(link)
+  const isInternalRead = /^\/read\/[\w\d]+/.test(link)
+  const targetURL = isInternalRead ? `https://getpocket.com${link}` : link
+  const url = new URL(targetURL)
   const isInternal = INTERNAL_DOMAINS.includes(url.hostname)
 
   if (isSyndicated) return CONTENT_OPEN_DESTINATION_INTERNAL
