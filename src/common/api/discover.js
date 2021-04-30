aa48818b585f120b39057c8d27520454e5f35717
Joel Kelly <joel@getpocket.com>
2021-04-30 13:54:20 -0700

    fix(discover): use rex for discover home

--- src/common/api/discover.js
+++ src/common/api/discover.js
@@ -1,14 +1,29 @@
 import { request } from 'common/utilities/request/request'
+import { GraphQLClient } from 'graphql-request'
+import { GRAPHQL_URL } from 'common/constants'
+import { TOPIC_IDS } from 'common/constants'
 
-/**
- * Get the explore home feed
- */
-export const getDiscoverFeed = (ssr) => {
+import getSlateLineup from 'common/api/graphql-queries/get-slate-lineup'
+
+export async function getDiscoverFeed(recommendationCount = 30) {
+  const variables = { id: TOPIC_IDS['explore'].id, recommendationCount }
+
   return request({
-    path: 'v3/discover',
-    ssr,
-    params: {
-      count: 20
-    }
+    api_url: GRAPHQL_URL,
+    path: 'graphql',
+    method: 'POST',
+    body: JSON.stringify({
+      query: getSlateLineup,
+      variables
+    })
   })
+    .then(processSlates)
+    .catch((error) => console.error(error))
+}
+
+function processSlates(response) {
+  const slates = response?.data?.getSlateLineup?.slates || []
+  const top = slates[0]?.recommendations
+  const bottom = slates[1]?.recommendations
+  return [...top, ...bottom]
 }
