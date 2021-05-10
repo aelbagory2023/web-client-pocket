7de1c7ce7bc223f225f6ea8d098e5a6529692374
Joel Kelly <joel@getpocket.com>
2021-05-10 16:38:52 -0700

    fix(discover): publisher click to open_url

--- src/components/item-card/card.js
+++ src/components/item-card/card.js
@@ -72,7 +72,7 @@ export const Card = (props) => {
     read_time,
     openExternal,
     syndicated,
-    original_url,
+    open_url,
     noImage
   } = item
 
@@ -170,7 +170,7 @@ export const Card = (props) => {
               //eslint-disable-next-line
               <a
                 className="publisher"
-                href={original_url}
+                href={open_url}
                 target="_blank"
                 onClick={onOpenOriginalUrl}
                 tabIndex={0}>
