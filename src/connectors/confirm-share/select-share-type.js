44a487e5715884c583e83ece05ba72b07004d534
Anthony Liddle <anthonyliddle@gmail.com>
2021-05-20 13:13:23 -0700

    fix(share): increase specificity for button styles

--- src/connectors/confirm-share/select-share-type.js
+++ src/connectors/confirm-share/select-share-type.js
@@ -3,43 +3,45 @@ import { buttonReset } from 'components/buttons/button-reset'
 import { Trans } from 'next-i18next'
 
 const tabAction = css`
-  width: 100%;
-  border-radius: 0;
-  text-align: center;
-  font-weight: 400;
-  padding: var(--size075) 0;
-  border-right: var(--dividerStyle);
-  border-bottom: var(--dividerStyle);
-  transition: none;
-
-  &:last-of-type {
-    border-right: none;
-  }
-  &:hover,
-  &:active {
-    background-color: var(--color-navCurrentTab);
-    color: initial;
-  }
-  &:focus {
+  button {
+    width: 100%;
+    border-radius: 0;
+    text-align: center;
+    font-weight: 400;
+    padding: var(--size075) 0;
+    border-right: var(--dividerStyle);
+    border-bottom: var(--dividerStyle);
     transition: none;
-    color: var(--color-navCurrentTabText);
-    outline: 1px auto var(--color-navCurrentTab);
-  }
 
-  &.active {
-    border-bottom-color: var(--color-popoverCanvas);
+    &:last-of-type {
+      border-right: none;
+    }
     &:hover,
     &:active {
-      cursor: initial;
-      background-color: initial;
+      background-color: var(--color-navCurrentTab);
       color: initial;
+    }
+    &:focus {
+      transition: none;
+      color: var(--color-navCurrentTabText);
+      outline: 1px auto var(--color-navCurrentTab);
+    }
+
+    &.active {
+      border-bottom-color: var(--color-popoverCanvas);
+      &:hover,
+      &:active {
+        cursor: initial;
+        background-color: initial;
+        color: initial;
+      }
     }
   }
 `
 
 export function SelectShareType({ active, activate }) {
   return (
-    <div className="tabs">
+    <div className={cx(tabAction, 'tabs')}>
       <ShareTypeTab active={active} activate={activate} tab="social">
         <Trans i18nKey="share:social-media">Social Media</Trans>
       </ShareTypeTab>
@@ -65,7 +67,7 @@ function ShareTypeTab({ active, activate, tab, children }) {
     <button
       data-cy={`share-tab-${tab}`}
       onClick={onClick}
-      className={cx(buttonReset, tabAction, isActive ? 'active' : null)}>
+      className={cx(buttonReset, isActive ? 'active' : null)}>
       {children}
     </button>
   )
