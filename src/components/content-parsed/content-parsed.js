0368796751058ade21842c48e425ed10199c0db0
Joel Kelly <joel@getpocket.com>
2021-05-25 13:55:28 -0700

    fix(syndicated): video sizing
    Signed-off-by: Joel Kelly <joel@getpocket.com>

--- src/components/content-parsed/content-parsed.js
+++ src/components/content-parsed/content-parsed.js
@@ -113,6 +113,25 @@ const contentWrapper = css`
     font-size: var(--fontSize125);
   }
 
+  .RIL_video {
+    position: relative;
+    padding-top: 56.25%;
+    margin-bottom: 2rem;
+
+    .player,
+    object {
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      width: 100%;
+      height: 100%;
+      border-radius: 8px;
+      overflow: hidden;
+    }
+  }
+
   .description,
   .ril_caption_content,
   .credit_content,
