8a833c661c915389d940eaa99c51522f133ed20d
Joel Kelly <joel@getpocket.com>
2021-05-25 13:07:47 -0700

    chore(collections): cleanup temporary endpoints
    Signed-off-by: Joel Kelly <joel@getpocket.com>

--- src/pages/api/getCollectionBySlug.js
+++
@@ -1,8 +0,0 @@
-const collectionData = require('./_data/getCollectionBySlug.json')
-
-export default function handler(req, res) {
-  const { slug } = req?.query
-  const response = collectionData[slug]?.data?.getCollectionBySlug
-  if (!response) return res.status(404)
-  res.status(200).json(JSON.stringify(response))
-}
--- src/pages/api/getCollections.js
+++
@@ -1,5 +0,0 @@
-const collectionData = require('./_data/getCollections.json')
-
-export default function handler(req, res) {
-  res.status(200).json(JSON.stringify(collectionData))
-}
