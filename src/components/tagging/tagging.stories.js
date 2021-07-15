7be13489e3ca49e26288bb96fa72fb0492410fb0
Aly Fluckey <afluckey@mozilla.com>
2021-07-15 17:43:56 -0500

    fix(tagging stories): fix tagging stories
    fix tagUpsell, it now renders
    fix tagSuggestionsLoading, previously it was showing the same as tagSuggestionsLoading
    remove isPremium props, doesn't do anything
    remove isPremium specific story

--- src/components/tagging/tagging.stories.js
+++ src/components/tagging/tagging.stories.js
@@ -3,13 +3,13 @@ import { TagList } from './tag.list'
 import { TagError } from './tag.error'
 import { TagUpsell } from './tag.upsell'
 import { TagSuggestions } from './tag.suggestions'
-import { COLORS } from 'mock/colors'
 
 export default {
   title: 'Components/Tagging'
 }
 
 const TAG_LIST = ['business', 'art', 'family', 'school', 'health']
+const SUGGESTED_TAG_LIST = ['elephants', 'tigers', 'family', 'health']
 
 export const tagList = () => <TagList tags={TAG_LIST} />
 
@@ -21,12 +21,10 @@ export const errorTag = () => <TagError />
 
 export const errorEmail = () => <TagError email />
 
-export const tagUpsell = () => <TagUpsell />
+export const tagUpsell = () => <TagUpsell onVisible={() => {}} />
+
+export const tagSuggestionsLoading = () => <TagSuggestions />
 
 export const tagSuggestions = () => (
-  <TagSuggestions isPremium suggestedTags={TAG_LIST} />
+  <TagSuggestions tags={TAG_LIST} suggestedTags={SUGGESTED_TAG_LIST} since={true} />
 )
-
-export const tagSuggestionsNoPremium = () => <TagSuggestions />
-
-export const tagSuggestionsLoading = () => <TagSuggestions isPremium />
