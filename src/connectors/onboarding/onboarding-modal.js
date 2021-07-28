91d332c70d81a94b59b75ae80117ce23c1f9eab7
Aly Fluckey <afluckey@mozilla.com>
2021-07-28 14:07:07 -0500

    fix(onboarding modal): add copy

--- src/connectors/onboarding/onboarding-modal.js
+++ src/connectors/onboarding/onboarding-modal.js
@@ -1,3 +1,4 @@
+import { css } from 'linaria'
 import { Button } from '@pocket/web-ui'
 import { Modal, ModalBody, ModalFooter } from 'components/modal/modal'
 import { useDispatch, useSelector } from 'react-redux'
@@ -5,6 +6,13 @@ import { useTranslation, Trans } from 'next-i18next'
 import { TopicSelector } from 'connectors/topic-list/topic-selector'
 import { onboardingCloseTopicSelectionModal } from './onboarding.state'
 
+const footerStyles = css`
+  margin-bottom: 0;
+  line-height: 2.5rem;
+  margin-right: 1rem;
+  font-weight: normal;
+  font-size: 1rem;
+`
 
 export const OnboardingModal = () => {
   const dispatch = useDispatch()
@@ -15,24 +23,28 @@ export const OnboardingModal = () => {
   const settingsFetched = useSelector((state) => state.settings.settingsFetched)
   const handleClose = () => dispatch(onboardingCloseTopicSelectionModal())
   const showModal = modalReady && settingsFetched
+  const firstName = useSelector((state) => state.user.first_name)
+  const title = firstName ? `Welcome to Pocket, ${firstName}!` : 'Welcome to Pocket!'
 
   return (
     <Modal
-      title="boop title"
+      title={title}
       appRootSelector={appRootSelector}
       isOpen={showModal}
-      screenReaderLabel="boop"
+      screenReaderLabel={title}
       handleClose={handleClose}>
       <ModalBody>
+        <p>Tell us what you're interested in to start curating your personal corner of the web</p>
         <TopicSelector />
       </ModalBody>
       <ModalFooter>
+        <p className={footerStyles}>Pick 3 to continue</p>
         <Button
           type="submit"
           data-cy="delete-confirm"
           onClick={handleClose}
           autoFocus={true}>
-          <Trans i18nKey="confirm:close">Close</Trans>
+          <Trans i18nKey="confirm:continue">Continue</Trans>
         </Button>
       </ModalFooter>
     </Modal>
