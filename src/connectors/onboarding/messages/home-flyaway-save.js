a558eb772ae1238885a584696cc19c666f28350b
Aly Fluckey <afluckey@mozilla.com>
2021-08-12 13:45:33 -0500

    fix(onboarding): hook save flyaway up to onboarding state, allow for dismissal of message

--- src/connectors/onboarding/messages/home-flyaway-save.js
+++ src/connectors/onboarding/messages/home-flyaway-save.js
@@ -1,14 +1,21 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { useTranslation } from 'next-i18next'
 import { Flyaway } from 'components/flyaway/flyaway'
 import { sendSnowplowEvent } from 'connectors/snowplow/snowplow.state'
+import { onboardingCloseSaveFlyaway } from '../onboarding.state'
 
 export const HomeFlyawaySave = () => {
   const dispatch = useDispatch()
   const { t } = useTranslation()
 
-  const showFlyaway = true
+  const pinnedTopics = useSelector((state) => state.settings.pinnedTopics)
+  const flyawayReady = useSelector((state) => state.onboarding.saveFlyaway)
+  const modalStatus = useSelector((state) => state.onboarding.topicSelectionModal)
+  const topicsSelected = pinnedTopics.length !== 0
+  const modalFinished = modalStatus === false
+  const showFlyaway = (topicsSelected && flyawayReady && modalFinished)
+
 
   useEffect(() => {
     if (showFlyaway) dispatch(sendSnowplowEvent('onboarding.flyaway.save.impression'))
@@ -16,6 +23,7 @@ export const HomeFlyawaySave = () => {
 
   const handleClose = () => {
     dispatch(sendSnowplowEvent('onboarding.flyaway.save.close'))
+    dispatch(onboardingCloseSaveFlyaway())
   }
 
   const title = t('onboarding:flyaway-save-title', 'Save articles you like')
@@ -23,6 +31,8 @@ export const HomeFlyawaySave = () => {
     'Home is filled with the best articles from across the web. Save one to read it later.')
 
   return (
-    <Flyaway title={title} description={description} handleClose={handleClose} />
+    ( showFlyaway ? 
+      <Flyaway title={title} description={description} handleClose={handleClose} />
+    : null )
   )
 }
--- src/connectors/onboarding/onboarding.state.js
+++ src/connectors/onboarding/onboarding.state.js
@@ -2,17 +2,20 @@ import { put, takeLatest } from 'redux-saga/effects'
 import { SETTINGS_FETCH_SUCCESS } from 'actions'
 import { SETTINGS_UPDATE } from 'actions'
 import { ONBOARDING_CLOSE_TOPICS_MODAL } from 'actions'
+import { ONBOARDING_CLOSE_SAVE_FLYAWAY } from 'actions'
 import { ONBOARDING_RESET } from 'actions'
 import { filterSettings } from 'common/utilities'
 
 const initialState = {
-  topicSelectionModal: true
+  topicSelectionModal: true,
+  saveFlyaway: true
 }
 
 /** ACTIONS
  --------------------------------------------------------------- */
 
 export const onboardingCloseTopicSelectionModal = () => ({ type: ONBOARDING_CLOSE_TOPICS_MODAL })
+export const onboardingCloseSaveFlyaway = () => ({ type: ONBOARDING_CLOSE_SAVE_FLYAWAY })
 export const resetOnboarding = () => ({ type: ONBOARDING_RESET })
 
 /** REDUCERS
@@ -32,6 +35,12 @@ export const onboardingReducers = (state = initialState, action) => {
       return {
         ...state,
         topicSelectionModal: false
+      }
+
+    case ONBOARDING_CLOSE_SAVE_FLYAWAY:
+      return {
+        ...state,
+        saveFlyaway: false
       }
 
     case ONBOARDING_RESET:
