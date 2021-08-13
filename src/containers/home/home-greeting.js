b02106a6e550fcd184c77ac3630bac62cb3084a3
Joel Kelly <joel@getpocket.com>
2021-08-13 13:30:04 -0700

    fix(greeting): revert greeting to an addendum

--- src/containers/home/home-greeting.js
+++ src/containers/home/home-greeting.js
@@ -1,19 +1,22 @@
 import { useSelector } from 'react-redux'
 import { getTimeOfDay } from 'common/utilities'
-import { CardPageHeader } from 'components/headers/discover-header'
+import { css } from 'linaria'
+
+const homeCollections = css`
+  font-family: 'Graphik Web';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1rem;
+  line-height: 1.2;
+  margin-bottom: 0;
+`
 
 export const HomeGreeting = () => {
   const firstName = useSelector((state) => state.user.first_name)
-  const pinnedTopics = useSelector((state) => state.settings.pinnedTopics)
   const timeOfDay = getTimeOfDay()
 
   const showName = firstName && firstName.length < 15
   const greeting = showName ? `Good ${timeOfDay}, ${firstName}!` : `Good ${timeOfDay}!`
 
-  const description =
-    pinnedTopics?.length === 0
-      ? `Select topics to see popular articles and our editors’ top picks.`
-      : null
-
-  return <CardPageHeader title={greeting} subHeading={description} />
+  return <div className={homeCollections}>{greeting}</div>
 }
