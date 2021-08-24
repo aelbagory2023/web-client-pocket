173ca1e6955e240ce1e05b9e4b9fb99cdece8e01
Aly Fluckey <afluckey@mozilla.com>
2021-08-24 16:44:00 -0500

    fix(onboarding): remove whitespace

--- src/containers/my-list/my-list.js
+++ src/containers/my-list/my-list.js
@@ -198,7 +198,7 @@ export default function MyList(props) {
             </>
           ) : (
             <CallOutBrand />
-            )}
+          )}
         </main>
       ) : null}
     </Layout>
