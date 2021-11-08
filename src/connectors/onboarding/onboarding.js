07b19b984ae6bd632d8db6b7175bc68bd3c6a6a5
Aly Fluckey <afluckey@mozilla.com>
2021-11-08 16:56:52 -0600

    fix(onboarding): showOnboarding if enrolled in onboarding.dev feature flag

--- src/connectors/onboarding/onboarding.js
+++ src/connectors/onboarding/onboarding.js
@@ -1,6 +1,6 @@
-// import { useTranslation } from 'next-i18next'
-// import { useSelector } from 'react-redux'
-// import { featureFlagActive } from 'connectors/feature-flags/feature-flags'
+import { useTranslation } from 'next-i18next'
+import { useSelector } from 'react-redux'
+import { featureFlagActive } from 'connectors/feature-flags/feature-flags'
 
 import { HomeWelcomeModal } from './messages/home-welcome-modal'
 import { HomeFlyawaySave } from './messages/home-flyaway-save'
@@ -10,15 +10,15 @@ import { MyListFlyawayExtensions } from './messages/my-list-flyaway-extensions'
 import { ReaderFlyawayApps } from './messages/reader-flyaway-apps'
 
 export const Onboarding = ({ type, ...rest }) => {
-  // const translations = useTranslation()
-  // const { i18n } = translations
-  // const { language: currentLanguage } = i18n
+  const translations = useTranslation()
+  const { i18n } = translations
+  const { language: currentLanguage } = i18n
 
-  // const featureState = useSelector((state) => state.features)
-  // const onboardingDev = featureFlagActive({ flag: 'onboarding.dev', featureState })
+  const featureState = useSelector((state) => state.features)
+  const onboardingDev = featureFlagActive({ flag: 'onboarding.dev', featureState })
   // const onboardingRelease = featureFlagActive({ flag: 'onboarding.release', featureState })
-  // const enrolled = onboardingDev || onboardingRelease
-  const showOnboarding = false // currentLanguage === 'en' && enrolled
+  const enrolled = onboardingDev
+  const showOnboarding = currentLanguage === 'en' && enrolled
 
   const onboardingTypes = {
     'home.modal': HomeWelcomeModal,
