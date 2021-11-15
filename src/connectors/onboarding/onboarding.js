b0bb5a3aaee16305de9b0ce6311d1db937380acd
Aly Fluckey <afluckey@mozilla.com>
2021-11-15 17:08:34 -0600

    fix(onboarding): remove locale check, add onboardingRelease check

--- src/connectors/onboarding/onboarding.js
+++ src/connectors/onboarding/onboarding.js
@@ -1,8 +1,6 @@
-import { useTranslation } from 'next-i18next'
 import { useSelector } from 'react-redux'
 import { featureFlagActive } from 'connectors/feature-flags/feature-flags'
 
-import { HomeWelcomeModal } from './messages/home-welcome-modal'
 import { HomeFlyawaySave } from './messages/home-flyaway-save'
 import { HomeFlyawayMyList } from './messages/home-flyaway-my-list'
 import { MyListFlyawayReader } from './messages/my-list-flyaway-reader'
@@ -10,18 +8,13 @@ import { MyListFlyawayExtensions } from './messages/my-list-flyaway-extensions'
 import { ReaderFlyawayApps } from './messages/reader-flyaway-apps'
 
 export const Onboarding = ({ type, ...rest }) => {
-  const translations = useTranslation()
-  const { i18n } = translations
-  const { language: currentLanguage } = i18n
-
   const featureState = useSelector((state) => state.features)
   const onboardingDev = featureFlagActive({ flag: 'onboarding.dev', featureState })
-  // const onboardingRelease = featureFlagActive({ flag: 'onboarding.release', featureState })
-  const enrolled = onboardingDev
-  const showOnboarding = currentLanguage === 'en' && enrolled
+  const onboardingRelease = featureFlagActive({ flag: 'onboarding.release', featureState })
+
+  const showOnboarding = onboardingDev || onboardingRelease
 
   const onboardingTypes = {
-    'home.modal': HomeWelcomeModal,
     'home.flyaway.save': HomeFlyawaySave,
     'home.flyaway.my-list': HomeFlyawayMyList,
     'my-list.flyaway.reader': MyListFlyawayReader,
