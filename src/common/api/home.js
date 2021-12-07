3f7c766526eb5c6cfbf3c2f8666530ca38406b92
Joel Kelly <joel@getpocket.com>
2021-12-07 14:53:24 -0800

    fix(home): derive items with new derivers

--- src/common/api/home.js
+++ src/common/api/home.js
@@ -1,7 +1,8 @@
 import { requestGQL } from 'common/utilities/request/request'
-import { getRecIds, arrayToObject } from 'common/utilities'
+import { arrayToObject } from 'common/utilities'
 import getSlateLineup from 'common/api/graphql-queries/get-slate-lineup'
 import { slateMeta } from 'common/slate-meta'
+import { deriveRecommendation } from 'common/api/derivers/item'
 
 const personalized = '05027beb-0053-4020-8bdc-4da2fcc0cb68'
 // const unpersonalized = '249850f0-61c0-46f9-a16a-f0553c222800'
@@ -19,22 +20,23 @@ export async function getHomeLineup({ recommendationCount = 5 }) {
 
 function processLineup(response) {
   const isPersonalized = response?.data?.getSlateLineup.id === homeLineup
+  const responseData = response?.data?.getSlateLineup
+  const { slateLineupExperiment, slateLineupRequestId, slateLineupId, slates } = responseData
+  const lineupAnalytics = { slateLineupExperiment, slateLineupRequestId, slateLineupId }
 
-  const slateLineup = getRecIds(response?.data?.getSlateLineup)
-  const slatesResponse = response?.data?.getSlateLineup.slates
-  const itemsById = getRecsById(slatesResponse, slateLineup)
-  const slatesById = processSlates(slatesResponse, isPersonalized)
+  const itemsById = getRecsById(slates, lineupAnalytics)
+  const slatesById = processSlates(slates, isPersonalized)
 
   const generalSlates = Object.keys(slatesById).filter((id) => slatesById[id].type !== 'topic')
   const topicSlates = Object.keys(slatesById).filter((id) => slatesById[id].type === 'topic')
 
-  return { generalSlates, topicSlates, slatesById, itemsById, slateLineup, isPersonalized }
+  return { generalSlates, topicSlates, slatesById, itemsById, isPersonalized }
 }
 
-function getRecsById(slates, slateLineup) {
+function getRecsById(slates, lineupAnalytics) {
   return slates.reduce((accumulator, current) => {
-    const recommendations = deriveItems(current, slateLineup)
-    const recsById = arrayToObject(recommendations, 'item_id')
+    const recommendations = deriveItems(current, lineupAnalytics)
+    const recsById = arrayToObject(recommendations, 'itemId')
     return { ...accumulator, ...recsById }
   }, {})
 }
@@ -49,54 +51,22 @@ function processSlates(slates, isPersonalized) {
         }
       : undefined
   })
-  return arrayToObject(slateWithIds, 'id')
+  return arrayToObject(slateWithIds, 'slateId')
 }
 
-export function deriveItems(slate, slateLineup) {
-  /**
-   * @title {string} The most appropriate title to show
-   * @thumbnail {url} The most appropriate image to show as a thumbnail
-   * @domain {string} The best text to display as the domain or publisher of this item
-   * @excerpt {string} The most appropriate excerpt to show
-   * @open_url {url} The url that should be saved or opened
-   * @share_url {url} The url that should be shared if the user shares this item
-   * @read_time {string} An approximation of how long it takes to read the article based on
-   * @save_status {string} A string value (unsaved, saving, saved)
-   * @recommendation_id {string} A string value needed for snowplow recommendation entity
-   * @slateLineup {object} A object needed for snowplow slate-lineup entity
-   * @slate {object} A object needed for snowplow slate entity
-   */
+export function deriveItems(slate, lineupAnalytics) {
   const recommendations = slate?.recommendations
+  const { slateId, slateRequestId, slateExperimentId, displayName, description } = slate
   const analyticsData = {
-    slateLineupId: slateLineup?.id,
-    slateLineupRequestId: slateLineup?.requestId,
-    slateLineupExperiment: slateLineup?.experimentId,
-    slateId: slate?.id,
-    slateRequestId: slate?.requestId,
-    slateExperiment: slate?.experimentId
+    ...lineupAnalytics,
+    slateId,
+    slateRequestId,
+    slateExperimentId,
+    displayName,
+    description
   }
 
-  return recommendations.map((recommendation) => {
-    return {
-      resolved_id: recommendation.item?.resolvedId,
-      item_id: recommendation.item?.itemId,
-      title: displayTitle(recommendation),
-      thumbnail: displayThumbnail(recommendation),
-      publisher: displayPublisher(recommendation),
-      excerpt: displayExcerpt(recommendation),
-      save_url: saveUrl(recommendation),
-      open_url: openUrl(recommendation),
-      original_url: originalUrl(recommendation),
-      resolved_url: recommendation.item?.resolvedUrl,
-      syndicated: syndicated(recommendation),
-      openExternal: false,
-      save_status: 'unsaved',
-      analyticsData: {
-        ...analyticsData,
-        recommendationId: recommendation.id || recommendation.item?.resolved_id
-      }
-    }
-  })
+  return recommendations.map((item) => deriveRecommendation(item, analyticsData))
 }
 
 export function deriveSlate(slate) {
@@ -133,88 +103,4 @@ function derivedMeta(currentMeta, slate) {
     type: currentMeta?.type || null,
     topicSlug: currentMeta?.slug || false
   }
-}
-
-/** TITLE
- * @param {object} feedItem An unreliable item returned from a v3 feed endpoint
- * @returns {string} The most appropriate title to show
- */
-function displayTitle({ item, curatedInfo }) {
-  return curatedInfo?.title || item?.title || item?.normalUrl || null
-}
-
-/** THUMBNAIL
- * @param {object} feedItem An unreliable item returned from a v3 feed endpoint
- * @returns {string:url} The most appropriate image to show as a thumbnail
- */
-
-function displayThumbnail({ item, curatedInfo }) {
-  return item?.collection?.imageUrl || curatedInfo?.imageSrc || item?.topImageUrl || false
-}
-
-/** PUBLISHER
- * @param {object} feedItem An unreliable item returned from a v3 feed endpoint
- * @returns {string} The best text to display as the publisher of this item
- */
-function displayPublisher({ item, publisher }) {
-  return item?.syndicatedArticle?.publisher?.name || item.domainMetadata?.name || publisher
-}
-
-/** EXCERPT
- * @param {object} feedItem An unreliable item returned from a v3 feed endpoint
- * @returns {string} The most appropriate excerpt to show
- */
-function displayExcerpt({ item, curatedInfo }) {
-  return curatedInfo?.excerpt || item?.excerpt || null
-}
-
-/** OPEN URL
- * @param {object} feedItem An unreliable item returned from a v3 feed endpoint
- * @returns {string} The url that should be saved or opened
- * When to use given_url or item_id:
- * When using it as an identifier with the API (actions, saving, etc)
- * When opening an item in a web browser
- * When sharing a link
- * NOT as a display url, unless no resolved_url exists
- * NOT as the display domain, unless no resolved_url exists
- *
- * When to use resolved_url or resolved_id:
- * When displaying a url to a user
- * When displaying a domain to a user
- * NOT when communicating with the API or for saving
- * NOT for opening in a web browser
- *
- * Do NOT use the following for syncing or opening content:
- * normal_urls - They are meant for internal database syncing only
- * resolved_ids - They are just links to resolved_urls
- */
-function openUrl({ item }) {
-  const url = originalUrl({ item })
-  if (url) return `${url}?utm_source=pocket_discover`
-  return false
-}
-
-/** SAVE URL
- * @param {object} feedItem An unreliable item returned from a v3 feed endpoint
- * @returns {string} The url that should be saved or opened
- */
-function saveUrl({ item }) {
-  return item?.givenUrl || false
-}
-
-/** OPEN_ORIGINAL
- * @param {object} feedItem An unreliable item returned from a v3 feed endpoint
- * @returns {string} The url that should be opened when visiting the live page
- */
-function originalUrl({ item }) {
-  return item?.givenUrl || false
-}
-
-/**
- * SYNDICATION
- * @param {object} feedItem An unreliable item returned from a v3 feed endpoint
- * @returns {bool} if the item is syndicated or not
- */
-const syndicated = function ({ item }) {
-  return !!item?.syndicatedArticle
 }
