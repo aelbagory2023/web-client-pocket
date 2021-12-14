c81db8b021c95bdeffe2592edd7577fe5a571571
Joel Kelly <joel@getpocket.com>
2021-12-14 12:55:01 -0800

    Revert "fix(tagging): adjust reconciler on tag action"
    This reverts commit 737b5c5ebf08623964b587e54191a876bca9774b.

--- src/connectors/items-by-id/my-list/items.state.js
+++ src/connectors/items-by-id/my-list/items.state.js
@@ -103,11 +103,11 @@ const reconcileActions = function (state, actions) {
     if (action === 'archive') stateDraft[item_id].isArchived = true
     if (action === 'unarchive') stateDraft[item_id].isArchived = false
     if (action === 'tags_replace') {
-      stateDraft[item_id].tags = getTagsArray(tags)
+      stateDraft[item_id].tags = getTagsObject(item_id, tags)
     }
     if (action === 'tags_add') {
       const current = stateDraft[item_id].tags || []
-      stateDraft[item_id].tags = [...current, ...getTagsArray(tags)]
+      stateDraft[item_id].tags = { ...current, ...getTagsObject(item_id, tags) }
     }
 
     if (action === 'delete') delete stateDraft[item_id]
@@ -116,7 +116,7 @@ const reconcileActions = function (state, actions) {
   return stateDraft
 }
 
-const getTagsArray = function (tags) {
-  const tagsArray = tags.map((tag) => ({ name: tag }))
-  return tagsArray
+const getTagsObject = function (item_id, tags) {
+  const tagsArray = tags.map((tag) => ({ item_id, tag }))
+  return arrayToObject(tagsArray, 'tag')
 }
