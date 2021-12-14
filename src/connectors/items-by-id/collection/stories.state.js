21c6d495d3488093e39c67e0496b95561ae887b9
Joel Kelly <joel@getpocket.com>
2021-12-14 12:59:28 -0800

    Revert "fix(collection): use new deriver for stories"
    This reverts commit 6c9388cd6632de117399a683f8ddf0fc0ec325d4.

--- src/connectors/items-by-id/collection/stories.state.js
+++ src/connectors/items-by-id/collection/stories.state.js
@@ -1,6 +1,6 @@
 import { put, takeEvery } from 'redux-saga/effects'
 import { saveItem as saveItemAPI } from 'common/api/saveItem'
-import { deriveStory } from 'common/api/derivers/item'
+import { deriveCollectionStories } from './stories.derive.js'
 import { arrayToObject } from 'common/utilities'
 
 import { COLLECTION_STORIES_REQUEST } from 'actions'
@@ -55,10 +55,9 @@ export const collectionStoriesReducers = (state = initialState, action) => {
 
     // SPECIAL HYDRATE:  This is sent from the next-redux wrapper and
     // it represents the state used to build the page on the server.
-    case HYDRATE: {
+    case HYDRATE:
       const { collectionStoriesById } = action.payload
       return { ...state, ...collectionStoriesById }
-    }
 
     default:
       return state
@@ -102,8 +101,8 @@ function* storiesSaveRequest(action) {
 function* storiesDerive(action) {
   try {
     const { stories } = action
-    const derivedStories = stories.map((story) => deriveStory(story))
-    const storiesById = arrayToObject(derivedStories, 'itemId')
+    const derivedStories = deriveCollectionStories(stories)
+    const storiesById = arrayToObject(derivedStories, 'item_id')
 
     yield put({ type: COLLECTION_STORIES_HYDRATE_SUCCESS, storiesById })
   } catch (error) {
