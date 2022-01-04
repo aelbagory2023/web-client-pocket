28ae677164f15947ced64cbf0fc9ed8813f8b2d6
Joel Kelly <joel@getpocket.com>
2022-01-04 10:17:13 -0800

    fix(unleash): recit query shape

--- src/common/api/unleash.js
+++ src/common/api/unleash.js
@@ -16,7 +16,7 @@ export async function getUnleash(sessionId, userId, birth, appName, locale, user
     accountCreatedAt: birth,
     appName,
     locale,
-    recItUserProfile: JSON.stringify({ userModels })
+    recItUserProfile: { userModels }
   }
   const url = UNLEASH_API_URL
   const client = new GraphQLClient(url)
