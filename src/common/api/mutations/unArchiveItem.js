8c39e77162f5df24721da558f9b1c1d2e0eebaa5
Joel Kelly <joel@getpocket.com>
2022-03-14 10:34:33 -0700

    doc(unarchive): note for mutation expectation

--- src/common/api/mutations/unArchiveItem.js
+++ src/common/api/mutations/unArchiveItem.js
@@ -20,6 +20,9 @@ const itemUnArchiveQuery = gql`
   }
 `
 
+// ?? NOTE:  THis is not functionality we currently use in web-client
+// ?? the current (3/2022) expectations is that re-adding move the item
+// ?? to the top of the users list.  This is achieved with the upsert mutation
 export function itemUnArchive(itemId) {
   return requestGQL({
     query: itemUnArchiveQuery,
