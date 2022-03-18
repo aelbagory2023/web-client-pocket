79d1ab20d0f2afc22a6163458485cc0bad597f85
Aly Fluckey <afluckey@mozilla.com>
2022-03-18 16:11:55 -0500

    chore(reader): move article styles out so they can be shared

---
+++ src/containers/read/article-styles.js
@@ -0,0 +1,52 @@
+import { css } from 'linaria'
+
+export const articleWrapperStyles = css`
+  p {
+    font-size: unset !important;
+  }
+
+  display: flex;
+  flex-direction: row;
+  padding: var(--spacing400) 0;
+  overflow-x: hidden;
+
+  .sidebar-anchor {
+    position: relative;
+    width: 0;
+    transition: width 150ms ease-in-out;
+    &.active {
+      width: 350px;
+    }
+  }
+
+  .reader {
+    padding: 0 var(--spacing250);
+    margin: var(--spacing250) auto;
+    width: 100%;
+  }
+
+  .is-video {
+    max-width: initial !important;
+    width: 80%;
+  }
+
+  @media print {
+    display: block;
+    padding: 0;
+
+    .sidebar-anchor {
+      display: none;
+    }
+
+    .reader {
+      padding: 0;
+      margin: 0;
+      max-width: unset !important;
+    }
+
+    :after {
+      margin-top: 1rem;
+      content: 'Printed with ❤️ from Pocket';
+    }
+  }
+`
