465210ae98a55d898bba83d1189bb87db002ccaf
Joel Kelly <joel@getpocket.com>
2022-05-17 11:36:09 -0700

    fix(react testing): rollback for react17 supoport

--- src/components/global-nav/tools/search/global-nav-search.spec.js
+++ src/components/global-nav/tools/search/global-nav-search.spec.js
@@ -16,45 +16,41 @@ describe('GlobalNavSearch', () => {
     expect(getByPlaceholderText(baseProps.placeholder))
   })
 
-  it('calls the onSubmit callback when the form is submitted', async () => {
-    const user = userEvent.setup()
+  it('calls the onSubmit callback when the form is submitted', () => {
     const mockSubmit = jest.fn()
     const { getByCy } = wrappedRender(
       <GlobalNavSearch {...baseProps} onSubmit={mockSubmit} value="for a moment like this" />
     )
-    await user.click(getByCy('search-submit'))
+    userEvent.click(getByCy('search-submit'))
     expect(mockSubmit).toHaveBeenCalledWith('for a moment like this')
   })
 
-  it('does not render a close button when no onClose is provided', async () => {
+  it('does not render a close button when no onClose is provided', () => {
     const { queryByCy } = wrappedRender(<GlobalNavSearch {...baseProps} />)
     expect(queryByCy('search-close')).toBeFalsy()
   })
 
-  it('calls the onClose callback when the Close button is clicked', async () => {
-    const user = userEvent.setup()
+  it('calls the onClose callback when the Close button is clicked', () => {
     const mockClose = jest.fn()
     const { getByCy } = wrappedRender(<GlobalNavSearch {...baseProps} onClose={mockClose} />)
-    await user.click(getByCy('search-cancel'))
+    userEvent.click(getByCy('search-cancel'))
     expect(mockClose).toHaveBeenCalled()
   })
 
-  it('calls the onFocus callback when the search input is focused, passing the Event object', async () => {
+  it('calls the onFocus callback when the search input is focused, passing the Event object', () => {
     const mockFocus = jest.fn()
-    const user = userEvent.setup()
     wrappedRender(<GlobalNavSearch {...baseProps} onFocus={mockFocus} />)
 
-    await user.tab()
+    userEvent.tab()
     expect(mockFocus).toHaveBeenCalled()
   })
 
-  it('calls the onBlur callback when the search input is blurred, passing the Event object', async () => {
+  it('calls the onBlur callback when the search input is blurred, passing the Event object', () => {
     const mockBlur = jest.fn()
-    const user = userEvent.setup()
     wrappedRender(<GlobalNavSearch {...baseProps} onFocus={mockBlur} />)
 
-    await user.tab()
-    await user.tab()
+    userEvent.tab()
+    userEvent.tab()
     expect(mockBlur).toHaveBeenCalled()
   })
 
