6af79e42d23a56399d366584c82557eb564406a3
Joel Kelly <joel@getpocket.com>
2022-05-20 14:30:29 -0700

    refactor(icons): move source files to top level

---
+++ icons/_iconStyle.js
@@ -0,0 +1,13 @@
+import { css } from 'linaria'
+
+export const iconStyle = css`
+  display: inline-block;
+  height: 1em;
+  line-height: 0;
+  vertical-align: middle;
+  margin-top: -0.25em;
+
+  svg {
+    height: 100%;
+  }
+`
