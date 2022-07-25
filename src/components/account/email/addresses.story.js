6e94ff5724a7b7af4815dac83c26bd7b222ed1d4
Joel Kelly <joel@getpocket.com>
2022-07-25 10:19:44 -0700

    fix(account): email truncation on account page

---
+++ src/components/account/email/addresses.story.js
@@ -0,0 +1,30 @@
+import { css, cx } from 'linaria'
+import { EmailAddresses as EmailAddressesComponent } from './addresses'
+import { accountStyles } from '../account'
+
+export default {
+  title: 'Components/EmailAddresses'
+}
+
+const noop = () => {}
+export const EmailAddresses = () => {
+  const aliases = {
+    'thisistheevenlongestemail@wowwhyisthisolongImeanreallythisiscrazy.com': {},
+    'thelongestemailisevenlongerthattheotheremails@wowwhyisthisolongImeanreallythisiscrazy.com': {}
+  }
+  return (
+    <div className={accountStyles}>
+      <EmailAddressesComponent
+        primaryEmail="thisisthelongestemail@wowwhyisthisolongImeanreallythisiscrazy.com"
+        onChangeEmail={noop}
+        onAddEmailAlias={noop}
+        onChangeEmailAlias={noop}
+        emailAlias={''}
+        emailAliasError={''}
+        onResendConfirmation={noop}
+        onRemoveAlias={noop}
+        aliases={aliases}
+      />
+    </div>
+  )
+}
