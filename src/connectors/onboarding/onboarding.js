78347dc181150a8b8d5f3f444dd9430eeb372a8c
Joel Kelly <joel@getpocket.com>
2022-08-04 09:35:59 -0700

    fix(onboarding): exclude user from onboarding if they are in setupv3

--- src/connectors/onboarding/onboarding.js
+++ src/connectors/onboarding/onboarding.js
@@ -7,12 +7,16 @@ import { HomeFlyawayMyList } from './messages/home-flyaway-my-list'
 import { MyListFlyawayReader } from './messages/my-list-flyaway-reader'
 import { MyListFlyawayExtensions } from './messages/my-list-flyaway-extensions'
 import { ReaderFlyawayApps } from './messages/reader-flyaway-apps'
+import { featureFlagActive } from 'connectors/feature-flags/feature-flags'
 
 export const Onboarding = ({ type, ...rest }) => {
   const userBirth = useSelector((state) => state.user.birth)
   const settingsFetched = useSelector((state) => state.settings.settingsFetched)
+  const featureState = useSelector((state) => state.features) || {}
   const eligible = eligibleUser(userBirth, START_DATE_FOR_ONBOARDING)
-  const showOnboarding = settingsFetched && eligible
+
+  const inSetupV3 = featureFlagActive({ flag: 'setup.moment.v3', featureState })
+  const showOnboarding = settingsFetched && eligible && !inSetupV3
 
   const onboardingTypes = {
     'home.flyaway.save': HomeFlyawaySave,
