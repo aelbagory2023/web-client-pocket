052590044f15cdadde7d51af0df9a7f055f00343
Joel Kelly <joel@getpocket.com>
2022-10-13 13:56:45 -0700

    home(recent saves): clean up recent saves

--- src/containers/home/home-recent-saves.js
+++ src/containers/home/home-recent-saves.js
@@ -6,8 +6,6 @@ import { getRecentSaves } from 'containers/home/home.state'
 import { RecentCard } from 'connectors/item-card/home/card-recent'
 import { FlexList } from 'components/items-layout/list-flex'
 import { sendSnowplowEvent } from 'connectors/snowplow/snowplow.state'
-import { featureFlagActive } from 'connectors/feature-flags/feature-flags'
-import { CardNext } from 'components/item-card/card-next'
 
 export const HomeRecentSaves = () => {
   const { t } = useTranslation()
@@ -15,10 +13,6 @@ export const HomeRecentSaves = () => {
   const recentSaves = useSelector((state) => state.home.recentSaves)
   const count = recentSaves.length
   const showExcerpt = count < 2
-  const featureState = useSelector((state) => state.features) || {}
-
-  const cardNext = featureFlagActive({ flag: 'home.next', featureState })
-  const CardToRender = cardNext ? RecentCardNext : RecentCard
 
   const onLinkClick = () => dispatch(sendSnowplowEvent('home.recent.view-my-list'))
 
@@ -27,15 +21,10 @@ export const HomeRecentSaves = () => {
     dispatch(getRecentSaves())
   }, [dispatch])
 
-  const homeNext = featureFlagActive({ flag: 'home.next', featureState })
-  const sectionTitle = homeNext
-    ? t('home:recent-saves', 'Recent Saves')
-    : t('home:recent-saves-title', 'Recent Saves to My List')
-
   return recentSaves?.length > 0 ? (
     <>
       <HomeHeader
-        headline={sectionTitle}
+        headline={t('home:recent-saves-title', 'Recent Saves to My List')}
         moreLinkText={t('home:recent-saves-my-list-link-text', 'Go to My List')}
         moreLinkUrl={'/my-list?src=recent-saves'}
         moreLinkClick={onLinkClick}
@@ -45,7 +34,7 @@ export const HomeRecentSaves = () => {
         items={recentSaves}
         offset={0}
         count={3}
-        ItemCard={CardToRender}
+        ItemCard={RecentCard}
         cardShape="flex"
         showExcerpt={showExcerpt}
         border={false}
@@ -53,61 +42,5 @@ export const HomeRecentSaves = () => {
         dataCy="recent-saves"
       />
     </>
-  ) : null
-}
-
-const RecentCardNext = ({ id, position, showMedia = true, showExcerpt = false }) => {
-  const dispatch = useDispatch()
-
-  // Get data from state
-  const item = useSelector((state) => state.myListItemsById[id])
-  const impressionFired = useSelector((state) => state.analytics.impressions.includes(id))
-
-  if (!item) return null
-
-  const { itemId, openExternal, readUrl, externalUrl } = item
-  const openUrl = readUrl && !openExternal ? readUrl : externalUrl
-  const analyticsData = {
-    id,
-    position,
-    destination: !openExternal ? 'internal' : 'external',
-    ...item?.analyticsData
-  }
-
-  /**
-   * ITEM TRACKING
-   * ----------------------------------------------------------------
-   */
-  const onOpenOriginalUrl = () => {
-    const data = { ...analyticsData, destination: 'external' }
-    dispatch(sendSnowplowEvent('home.recent.view-original', data))
-  }
-  const onOpen = () => dispatch(sendSnowplowEvent('home.recent.open', analyticsData))
-  const onImpression = () => dispatch(sendSnowplowEvent('home.recent.impression', analyticsData))
-  const onItemInView = (inView) => (!impressionFired && inView ? onImpression() : null)
-
-  const itemImage = item?.noImage ? '' : item?.thumbnail
-  const {tags, title, publisher, excerpt, timeToRead, isSyndicated, isInternalItem, fromPartner } = item //prettier-ignore
-
-  return item ? (
-    <CardNext
-      itemId={itemId}
-      externalUrl={externalUrl}
-      tags={tags}
-      title={title}
-      itemImage={itemImage}
-      publisher={publisher}
-      excerpt={excerpt}
-      timeToRead={timeToRead}
-      isSyndicated={isSyndicated}
-      isInternalItem={isInternalItem}
-      fromPartner={fromPartner}
-      className="side-by-side"
-      openUrl={openUrl}
-      // Tracking
-      onItemInView={onItemInView}
-      onOpen={onOpen}
-      onOpenOriginalUrl={onOpenOriginalUrl}
-    />
   ) : null
 }
