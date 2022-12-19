3392cc7942571bb2a46f46357ccfda669021b419
Joel Kelly <joel@getpocket.com>
2022-12-19 15:23:24 -0800

    chore(coming-soon): cleanup unusued

--- src/containers/coming-soon/coming-soon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Layout from 'layouts/mobile-web'
-export default function ComingSoon() {
-  return (
-    <Layout>
-      <h1>Coming Soon</h1>
-    </Layout>
-  )
-}
