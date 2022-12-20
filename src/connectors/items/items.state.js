0bb4edf49d89655fd7f8d75fc71cdf9828804ae5
Joel Kelly <joel@getpocket.com>
2022-12-19 16:00:57 -0800

    chore(saved): relocate set-no-image

--- src/connectors/items/items.state.js
+++ src/connectors/items/items.state.js
@@ -1,5 +1,10 @@
 import { ITEMS_SUCCESS } from 'actions'
 import { READ_ITEM_SUCCESS } from 'actions'
+import { ITEMS_SET_NO_IMAGE } from 'actions'
+
+/** ACTIONS
+ --------------------------------------------------------------- */
+ export const setNoImage = (id) => ({ type: ITEMS_SET_NO_IMAGE, id })
 
 /** ITEM REDUCERS
  --------------------------------------------------------------- */
@@ -9,6 +14,14 @@ export const itemsReducers = (state = {}, action) => {
     case ITEMS_SUCCESS: {
       return { ...state, ...action.itemsById }
     }
+
+    case ITEMS_SET_NO_IMAGE: {
+      const { id } = action
+      const item = state[id]
+      const itemDraft = { ...item, noImage: true }
+      return { ...state, [id]: itemDraft }
+    }
+
     default:
       return state
   }
