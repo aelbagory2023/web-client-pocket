1c0e590909f569873e0b11a2234788b4fd8b40c1
Joseph Perez <joperez@dons.usfca.edu>
2023-01-11 14:06:58 -0800

    fix(tagging): re-focus input on invalid submission attempt

--- src/connectors/confirm/tag-edit.js
+++ src/connectors/confirm/tag-edit.js
@@ -1,6 +1,6 @@
 import { css } from 'linaria'
 import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { Button } from 'components/buttons/button'
 import { Modal, ModalBody, ModalFooter } from 'components/modal/modal'
 import { useDispatch, useSelector } from 'react-redux'
@@ -20,6 +20,8 @@ export const ConfirmTagEdit = () => {
   const router = useRouter()
   const { t } = useTranslation()
 
+  const inputReference = useRef(null);
+
   // Handle delete actions with confirmation
   const tagToEdit = useSelector((state) => state.userTags.tagToEdit)
 
@@ -27,7 +29,10 @@ export const ConfirmTagEdit = () => {
   const onChange = (event) => setValue(event.target.value)
 
   const showModal = tagToEdit !== false
-  const confirmTagEdit = () => dispatch(confirmEditUserTag(tagToEdit, value, router)) // prettier-ignore
+  const confirmTagEdit = () => {
+    if (!value.trim()) return inputReference.current.focus()
+    dispatch(confirmEditUserTag(tagToEdit, value, router)) // prettier-ignore
+  }
   const cancelTagEdit = () => dispatch(cancelEditUserTag())
 
   const appRootSelector = '#__next'
@@ -53,6 +58,7 @@ export const ConfirmTagEdit = () => {
             value={value}
             onChange={onChange}
             maxLength={MAX_TAG_NAME_LENGTH}
+            ref={inputReference}
           />
         </div>
       </ModalBody>
