33b433aa886ac350d1f4ef8fc9457fbd7bdbdd9c
Anthony Liddle <anthonyliddle@gmail.com>
2023-01-25 09:31:47 -0800

    fix(syndicated): updating identifier used to call rec events

--- src/containers/syndicated-article/pocket-recs.js
+++ src/containers/syndicated-article/pocket-recs.js
@@ -1,19 +1,21 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import RecsComponent from 'components/pocket-recs/pocket-recs'
 import { sendSnowplowEvent } from 'connectors/snowplow/snowplow.state'
 
 export const PocketRecs = () => {
+  const dispatch = useDispatch()
+
   const articleData = useSelector((state) => state.syndicatedArticle.articleData)
   const { relatedEndOfArticle } = articleData
 
   const handleRecImpression = ({ position, corpusRecommendationId, url }) => {
     const analyticsData = { position, corpusRecommendationId, url }
-    sendSnowplowEvent('syndicated.corpus.bottom.impression', analyticsData)
+    dispatch(sendSnowplowEvent('syndicated.rec.bottom.impression', analyticsData))
   }
 
   const handleRecClick = ({ position, corpusRecommendationId, url }) => {
     const analyticsData = { position, corpusRecommendationId, url }
-    sendSnowplowEvent('syndicated.corpus.bottom.open', analyticsData)
+    dispatch(sendSnowplowEvent('syndicated.rec.bottom.open', analyticsData))
   }
 
   return (
--- src/containers/syndicated-article/publisher-recs.js
+++ src/containers/syndicated-article/publisher-recs.js
@@ -1,19 +1,21 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { PublisherRecs as RecsComponent } from 'components/content-recs/publisher-recs'
 import { sendSnowplowEvent } from 'connectors/snowplow/snowplow.state'
 
 export const PublisherRecs = ({ publisher }) => {
+  const dispatch = useDispatch()
+
   const articleData = useSelector((state) => state.syndicatedArticle.articleData)
   const { relatedRightRail } = articleData
 
   const handleRecImpression = ({ position, corpusRecommendationId, url }) => {
     const analyticsData = { position, corpusRecommendationId, url }
-    sendSnowplowEvent('syndicated.corpus.sidebar.impression', analyticsData)
+    dispatch(sendSnowplowEvent('syndicated.rec.sidebar.impression', analyticsData))
   }
 
   const handleRecClick = ({ position, corpusRecommendationId, url }) => {
     const analyticsData = { position, corpusRecommendationId, url }
-    sendSnowplowEvent('syndicated.corpus.sidebar.open', analyticsData)
+    dispatch(sendSnowplowEvent('syndicated.rec.sidebar.open', analyticsData))
   }
 
   return (
