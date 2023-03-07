c74b20a2d4814ba5e055f937daab64c8938d16ae
Aly Fluckey <afluckey@mozilla.com>
2023-03-07 18:07:26 -0500

    fix(shareable-lists): add getShareableListPilotStatusQuery

---
+++ src/common/api/queries/get-shareable-lists-pilot-status.js
@@ -0,0 +1,16 @@
+import { gql } from 'graphql-request'
+import { requestGQL } from 'common/utilities/request/request'
+
+const getShareableListPilotStatusQuery = gql`
+  query GetShareableListPilotStatus($externalId: ID!) {
+    shareableListsPilotUser
+  }
+`
+export function getShareableListPilotStatus() {
+  return requestGQL({
+    query: getShareableListPilotStatusQuery,
+    operationName: 'GetShareableListPilotStatus'
+  })
+    .then((response) => response?.data?.shareableListsPilotUser)
+    .catch((error) => console.error(error))
+}
