1e138d3a4e157c688be8804a8fb4d31a26b4257f
Joseph Perez <joperez@dons.usfca.edu>
2023-03-16 14:22:01 -0700

    feat(gtm): added scripts to head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,18 @@ class ClientDocument extends Document {
       <Html>
         {/* prettier-ignore */}
         <Head>
+          {/* Google Tag Manager */}
+          <NextScript>
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P4LPJ42');
+            `}
+          </NextScript>
+          {/* End Google Tag Manager */}
+
           {/* Loads an iOS app store banner at the top of Safari */}
           <meta name="apple-itunes-app" content="app-id=309601447" />
 
@@ -72,6 +84,10 @@ class ClientDocument extends Document {
           />
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4LPJ42"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
