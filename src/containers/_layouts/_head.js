4f10bf3fce2ed5c677b50171508442c81da90a21
Anthony Liddle <anthonyliddle@gmail.com>
2023-03-21 11:00:44 -0700

    fix(head): remove data-cy attributes from meta tags to assist unfurling

--- src/containers/_layouts/_head.js
+++ src/containers/_layouts/_head.js
@@ -20,32 +20,32 @@ export const PocketHead = ({ metaData = {}, title: pageTitle, canonical, forceWe
 
       {forceWebView ? <meta name="x-pocket-force-webview"/> : null}
       {/*  Primary Meta Tags */}
-      <meta name="description" content={description} data-cy="meta-description" />
+      <meta name="description" content={description} />
 
       {/*  Pocket Specific Tags */}
-      <meta name="x-pocket-override-excerpt" content={description} data-cy="pkt-description" />
+      <meta name="x-pocket-override-excerpt" content={description} />
 
       {/* Schema.org for Google */}
-      <meta itemProp="name" content={title} data-cy="itemprop-name" />
-      <meta itemProp="description" content={description} data-cy="itemprop-description" />
-      {image ? <meta itemProp="image" content={imageCacheUrl} data-cy="itemprop-image" /> : null}
+      <meta itemProp="name" content={title} />
+      <meta itemProp="description" content={description} />
+      {image ? <meta itemProp="image" content={imageCacheUrl} /> : null}
 
       {/*  Twitter */}
-      <meta name="twitter:card" content={twitterCardType} data-cy="twitter-card" />
-      <meta name="twitter:url" content={url} data-cy="twitter-url" />
-      <meta name="twitter:title" content={title} data-cy="twitter-title" />
-      <meta name="twitter:description" content={description} data-cy="twitter-description" />
-      <meta name="twitter:site" content="@pocket" data-cy="twitter-site" />
-      {image ? <meta name="twitter:image" content={imageCacheUrl} data-cy="twitter-image" /> : null}
+      <meta name="twitter:card" content={twitterCardType} />
+      <meta name="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:site" content="@pocket" />
+      {image ? <meta name="twitter:image" content={imageCacheUrl} /> : null}
 
       {/* Open Graph general (Facebook, Pinterest) */}
-      <meta property="fb:app_id" content={FACEBOOK_APP_ID} data-cy="fb-app_id" />
-      <meta property="og:type" content={ogType} data-cy="og-type" />
-      <meta property="og:url" content={url} data-cy="og-url" />
-      <meta property="og:title" content={title} data-cy="og-title" />
-      <meta property="og:description" content={description} data-cy="og-description" />
-      <meta property="og:site_name" content="Pocket" data-cy="og-site_name" />
-      {image ? <meta property="og:image" content={imageCacheUrl} data-cy="og-image" /> : null}
+      <meta property="fb:app_id" content={FACEBOOK_APP_ID} />
+      <meta property="og:type" content={ogType} />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content="Pocket" />
+      {image ? <meta property="og:image" content={imageCacheUrl} /> : null}
 
       {/* Progressive Web App based on https://github.com/gokulkrishh/awesome-meta-and-manifest */}
       <meta charSet="utf-8" />
