203303a135912d1847e9d5767598f434c23ea0c3
Joel Kelly <joel@getpocket.com>
2023-04-10 10:18:00 -0700

    test(account): eligibility test using ISOString

---
+++ src/common/utilities/account/account.spec.js
@@ -0,0 +1,19 @@
+import { eligibleUser } from './eligible-user'
+
+const ISOAccountCreationsDate = '2015-10-22T09:49:47.000Z'
+const testStartBeforeDate = new Date('March 25, 2015 2:00:30 PM GMT-07:00')
+const testStartAfterDate = new Date('November 29, 2022 10:20:00 AM')
+
+describe('ElibibleUser', () => {
+  it('should return true if user started after the test start date', () => {
+    expect(eligibleUser(ISOAccountCreationsDate, testStartBeforeDate)).toBeTruthy()
+  })
+
+  it('should return false if user started before the test start date', () => {
+    expect(eligibleUser(ISOAccountCreationsDate, testStartAfterDate)).toBeFalsy()
+  })
+
+  it('should return false if user started at the same time as the start date', () => {
+    expect(eligibleUser(ISOAccountCreationsDate, ISOAccountCreationsDate)).toBeFalsy()
+  })
+})
