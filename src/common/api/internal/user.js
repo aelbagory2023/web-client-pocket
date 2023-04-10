b5e3ea1c15ad0d607e5641eaca1b8b09eacd7754
Joel Kelly <joel@getpocket.com>
2023-04-10 15:54:09 -0700

    Revert "fix(user): remove dangerous ignore ssl"
    This reverts commit 96b4e9549cb5d3df60324e328143e1594cd59e47.

--- src/common/api/internal/user.js
+++ src/common/api/internal/user.js
@@ -3,8 +3,7 @@ export async function getUser(access_token) {
   const isDev = process.env.NODE_ENV !== 'production'
 
   // !! DANGER: If we are not in production ignore ssl errors
-  // !! DO NOT MERGE WITH THIS LINE UNCOMMENTED.
-  // if (isDev) process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+  if (isDev) process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
   // On dev we want to hit the non-rewritten api endpoin
   const path = isDev
