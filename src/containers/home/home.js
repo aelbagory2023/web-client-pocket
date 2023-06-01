500d6bcf4f2ed01f050069c2f3d333d9c472aaf8
Joel Kelly <joel@getpocket.com>
2023-06-01 10:44:32 -0700

    fix(home): remove german lang banner

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -1,46 +1,19 @@
 import Layout from 'layouts/main'
-
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { HomeRecentSaves } from 'containers/home/recent-saves/recent-saves'
 import { HomeContent } from './content'
-
 import { SuccessFXA } from 'components/snackbar/success-fxa'
 import { HomeSetup } from './setup/setup'
-
-import { useRouter } from 'node_modules/next/router'
-import { BannerGermanHome } from 'components/banner/german-home'
 import { HomeGreeting } from './recent-saves/greeting'
 
-import { sendSnowplowEvent } from 'connectors/snowplow/snowplow.state'
 
 export const Home = ({ metaData }) => {
-  const dispatch = useDispatch()
-  const { locale } = useRouter()
-  const bannerLanguages = ['de', 'de-DE']
-  const showBanner = bannerLanguages.includes(locale)
-
-  const handleBannerImpression = () => {
-    dispatch(sendSnowplowEvent('banner.german.home.impression'))
-  }
-
-  const handleBannerClick = () => {
-    dispatch(sendSnowplowEvent('banner.german.home.click'))
-  }
-
   const userStatus = useSelector((state) => state.user.user_status)
-  const featureState = useSelector((state) => state.features) || {}
-
-  const shouldRender = userStatus !== 'pending' && featureState.flagsReady
+  const shouldRender = userStatus !== 'pending'
   if (!shouldRender) return null
 
   return (
     <Layout metaData={metaData} isFullWidthLayout={true} noContainer={true}>
-      {showBanner ? (
-        <BannerGermanHome
-          handleBannerImpression={handleBannerImpression}
-          handleBannerClick={handleBannerClick}
-        />
-      ) : null}
       <SuccessFXA type="home" />
 
       <HomeSetup />
