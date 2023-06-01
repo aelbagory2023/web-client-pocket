2a621ff9c926ab056f1efa46c390a9cb4fd28aa8
Joel Kelly <joel@getpocket.com>
2023-05-31 17:02:16 -0700

    build(stories) update annotations popover

--- src/components/popover/popover-selection.story.js
+++ src/components/popover/popover-selection.story.js
@@ -1,11 +1,12 @@
 import { SelectionPopover } from './popover-selection'
 import { useState } from 'react'
+import { css } from '@emotion/css'
 
 export default {
-  title: 'Article/Annotation Selection'
+  title: 'Article/Annotations'
 }
 
-export const Normal = () => {
+export const Selection = () => {
   const [highlight, setHighlight] = useState(null)
 
   const toggleHighlight = () => {
@@ -24,9 +25,17 @@ export const Normal = () => {
 
   return (
     <div onMouseUp={toggleHighlight}>
-      {highlight ? (
-        <SelectionPopover anchor={highlight} disablePopup={clearSelection} />
-      ) : null}
+      <div className={contentStyle}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto cumque necessitatibus
+        minus, commodi laudantium pariatur nesciunt veniam, animi et, dolore explicabo quis! Eveniet
+        porro deleniti id, laboriosam ipsam maiores temporibus?
+      </div>
+      {highlight ? <SelectionPopover anchor={highlight} disablePopup={clearSelection} /> : null}
     </div>
   )
 }
+
+const contentStyle = css`
+  margin-top: 6rem;
+  max-width: 400px;
+`
