73e33718ffc019c7c59e9d34fecf43e6c8783661
Anthony Liddle <anthonyliddle@gmail.com>
2024-02-05 13:46:07 -0800

    fix(bulk): updated the processing animation

--- src/components/processing/processing.js
+++ src/components/processing/processing.js
@@ -1,16 +1,48 @@
-import { ModalBody } from 'components/modal/modal'
 import { ProgressPill } from 'components/progress-pill/progress-pill'
 import { Trans } from 'next-i18next'
+import { css } from '@emotion/css'
+
+import { ProcessingAnimation } from './processing-animation'
+import { breakpointTinyTablet } from 'common/constants'
+
+const processingWrapper = css`
+  position: relative;
+  display: flex;
+
+  aside {
+    width: 86px;
+    margin: 0 16px 0 0;
+  }
+
+  main {
+    width: 100%;
+  }
+
+  p {
+    margin-bottom: 12px;
+  }
+
+  ${breakpointTinyTablet} {
+    flex-direction: column;
+
+    aside {
+      margin: 0 auto 16px;
+    }
+  }
+`
 
 export const BatchProcessing = ({ batchTotal, batchCount }) => {
   return (
-    <ModalBody>
-      <p>
-        <em>
-          <Trans i18nKey="confirm:processing">... Processing Items</Trans>
-        </em>
-      </p>
-      <ProgressPill total={batchTotal} current={batchTotal - batchCount} />
-    </ModalBody>
+    <div className={processingWrapper}>
+      <aside>
+        <ProcessingAnimation />
+      </aside>
+      <main>
+        <p>
+          <Trans i18nKey="confirm:new-processing">Hang tight, we’re making things happen</Trans> 😎
+        </p>
+        <ProgressPill total={batchTotal} current={batchTotal - batchCount} />
+      </main>
+    </div>
   )
 }
---
+++ src/containers/lists/list-individual/load-more.js
@@ -0,0 +1,65 @@
+import { useRef } from 'react'
+import { useIntersectionObserver } from 'common/utilities/intersection/intersection'
+import { useSelector } from 'react-redux'
+import { Loader } from 'components/loader/loader'
+import { css } from '@emotion/css'
+
+const loadMoreStyle = css`
+  grid-column: 1/-1;
+  height: 135px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--color-popoverCanvas);
+  border: 1px solid var(--color-popoverBorder);
+  border-radius: var(--borderRadius);
+  margin-top: 2rem;
+  font-family: var(--fontSansSerif);
+  & > div {
+    margin-left: 0.5rem;
+  }
+`
+const loadMoreRefStyle = css`
+  transform: translateY(-200%);
+  height: 350px;
+  width: 100%;
+  display: block;
+  pointer-events: none;
+  z-index: -1;
+`
+
+export function LoadMore({ loadMore }) {
+  const hasNextPage = useSelector((state) => state.pageListsInfo.hasNextPage)
+  const loading = useSelector((state) => state.pageListsInfo.loading)
+  const error = useSelector((state) => state.pageListsInfo.error)
+  const pageSavedIds = useSelector((state) => state.pageSavedIds)
+
+  const shouldLoadMore = hasNextPage && !loading
+
+  const loadMoreMessage = pageSavedIds.length ? 'Loading more items' : ''
+
+  return loading || error ? (
+    <div key="load-more" className={loadMoreStyle}>
+      {error ? (
+        <>Something went wrong while retrieving your items</>
+      ) : (
+        <>
+          {loadMoreMessage} <Loader />
+        </>
+      )}
+    </div>
+  ) : (
+    <LoadMoreTrigger loadMore={loadMore} shouldLoadMore={shouldLoadMore} />
+  )
+}
+
+function LoadMoreTrigger({ loadMore, shouldLoadMore }) {
+  const viewRef = useRef(null)
+
+  // Fire when item is in view
+  const entry = useIntersectionObserver(viewRef, { threshold: 0.5 })
+  const inView = !!entry?.isIntersecting
+  if (inView && shouldLoadMore) loadMore()
+
+  return <div ref={viewRef} className={loadMoreRefStyle} />
+}
