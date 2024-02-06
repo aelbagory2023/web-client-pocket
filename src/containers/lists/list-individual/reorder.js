fa9958e9a2f215dd2f45a8d2b7f466e3f0c3355d
Anthony Liddle <anthonyliddle@gmail.com>
2024-02-06 11:43:42 -0800

    fix(lists): add item pagination to reorder component

--- src/containers/lists/list-individual/reorder.js
+++ src/containers/lists/list-individual/reorder.js
@@ -1,17 +1,28 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { ReorderList } from 'components/reorder/reorder'
 import { ReorderItem } from 'components/reorder/reorder'
 import { ListSortHeader } from 'components/headers/lists-header'
 import { IndividualListReorderCard } from 'connectors/lists/individual-list.reorder.card'
 import { mutateReorderListItems } from 'connectors/lists/mutation-update.state'
 import { sendSnowplowEvent } from 'connectors/snowplow/snowplow.state'
+import { getMoreIndividualListAction } from '../lists.state'
+import { LoadMore } from './load-more'
 
 export const ListReorder = ({ id, toggleSort }) => {
   const dispatch = useDispatch()
 
   const { listItemIds, ...list } = useSelector((state) => state.listsDisplay[id])
-  const [items, setItems] = useState(listItemIds)
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    // they're the same, don't update anything
+    if (listItemIds.length === items.length) return () => {}
+
+    // combine and dedupe to preserve any changes
+    const newItems = [...new Set([...items, ...listItemIds])]
+    setItems(newItems)
+  }, [listItemIds, items])
 
   const { title, description, listItemNoteVisibility, analyticsData: passedAnalytics } = list
 
@@ -33,6 +44,8 @@ export const ListReorder = ({ id, toggleSort }) => {
 
   const updateList = (newItems) => setItems(newItems)
 
+  const loadMore = () => dispatch(getMoreIndividualListAction(id))
+
   return (
     <main className="main">
       <ListSortHeader
@@ -49,6 +62,8 @@ export const ListReorder = ({ id, toggleSort }) => {
           </ReorderItem>
         ))}
       </ReorderList>
+
+      <LoadMore loadMore={loadMore} />
     </main>
   )
 }
