2ccf55d521879a5680d3c96ec5bfd681c7815d14
Joel <joel.kelly@me.com>
2024-03-26 09:24:23 -0700

    feat(legal): reclaim privacy and tos

--- clients/web/src/pages/privacy/index.tsx
+++ clients/web/src/pages/privacy/index.tsx
@@ -1,20 +1,67 @@
-import type { NextPage } from 'next'
-import { PrivacyPolicyComponent } from 'containers/privacy-policy'
+import { LegalComponent as Component } from 'containers/legal'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { LOCALE_COMMON } from 'common/constants'
+import { readFileSync } from 'node:fs'
+import { marked } from 'marked'
+import customHeadingId from 'marked-custom-heading-id'
+
+// Override function
+const renderer = {
+  text(text) {
+    const hasDateTime = /\{\: datetime=/.test(text)
+    return hasDateTime ? '' : false
+  }
+}
 
 export async function getStaticProps({ locale }) {
+  const localeMap = {
+    'de-DE': 'de',
+    'en-KE': 'en',
+    'es-LA': 'es-ES',
+    es: 'es-ES',
+    'fr-CA': 'fr',
+    'fr-FR': 'fr',
+    'it-IT': 'it',
+    'ja-JP': 'ja',
+    'ko-KR': 'ko',
+    'nl-NL': 'nl',
+    'pl-PL': 'pl',
+    pt: 'pt-BR',
+    'pt-PT': 'pt-BR',
+    'ru-RU': 'ru',
+    zh: 'zh-CN'
+  }
+  const legalLocale = localeMap[locale] || locale
+  let mdContent
+
+  try {
+    mdContent = await readFileSync(
+      `node_modules/legal-docs/${legalLocale}/pocket_privacy_policy.md`,
+      'utf8'
+    )
+  } catch {
+    mdContent = await readFileSync(`node_modules/legal-docs/en/pocket_privacy_policy.md`, 'utf8')
+  }
+
+  const mdContentModified = mdContent.replace(/\{:\s?\#(.+\S)\s?\}/gi, '{#$1}')
+  const content = await marked.use(customHeadingId()).use({ renderer }).parse(mdContentModified)
+
   return {
     props: {
       ...(await serverSideTranslations(locale, [...LOCALE_COMMON])),
-      authRequired: true,
-      locale
+      authRequired: false,
+      locale,
+      content
     }
   }
 }
 
-const PrivacyPolicy: NextPage = () => {
-  return <PrivacyPolicyComponent />
+const PrivacyPolicy = ({ content }) => {
+  return (
+    <Component>
+      <div dangerouslySetInnerHTML={{ __html: content }}></div>
+    </Component>
+  )
 }
 
 export default PrivacyPolicy
--- clients/web/src/pages/tos/index.tsx
+++ clients/web/src/pages/tos/index.tsx
@@ -1,20 +1,63 @@
-import type { NextPage } from 'next'
-import { TermsOfService as Component } from 'containers/terms-of-service'
+import { LegalComponent as Component } from 'containers/legal'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { LOCALE_COMMON } from 'common/constants'
+import { readFileSync } from 'node:fs'
+import { marked } from 'marked'
+import customHeadingId from 'marked-custom-heading-id'
 
-export async function getStaticProps({ locale }) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, [...LOCALE_COMMON])),
-      authRequired: true,
-      locale
-    }
+// Override function
+const renderer = {
+  text(text) {
+    const hasDateTime = /\{\: datetime=/.test(text)
+    return hasDateTime ? '' : false
   }
 }
 
-const TermsOfService: NextPage = () => {
-  return <Component />
+export async function getStaticProps({ locale }) {
+  const localeMap = {
+    'de-DE': 'de',
+    'en-KE': 'en',
+    'es-LA': 'es-ES',
+    es: 'es-ES',
+    'fr-CA': 'fr',
+    'fr-FR': 'fr',
+    'it-IT': 'it',
+    'ja-JP': 'ja',
+    'ko-KR': 'ko',
+    'nl-NL': 'nl',
+    'pl-PL': 'pl',
+    pt: 'pt-BR',
+    'pt-PT': 'pt-BR',
+    'ru-RU': 'ru',
+    zh: 'zh-CN'
+  }
+  const legalLocale = localeMap[locale] || locale
+  let mdContent
+
+  try {
+    mdContent = await readFileSync(`node_modules/legal-docs/${legalLocale}/pocket_tos.md`, 'utf8')
+  } catch {
+    mdContent = await readFileSync(`node_modules/legal-docs/en/pocket_tos.md`, 'utf8')
+  }
+
+  const mdContentModified = mdContent.replace(/\{:\s?\#(.+\S)\s?\}/gi, '{#$1}')
+  const content = await marked.use(customHeadingId()).use({ renderer }).parse(mdContentModified)
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [...LOCALE_COMMON])),
+      authRequired: false,
+      locale,
+      content
+    }
+  }
+}
+const TermsOfService = ({ content }) => {
+  return (
+    <Component>
+      <div dangerouslySetInnerHTML={{ __html: content }}></div>
+    </Component>
+  )
 }
 
 export default TermsOfService
