6e0bdff276d9cb88e5d785f03ff6ec27868c6623
Joel <joel.kelly@me.com>
2024-04-01 10:56:10 -0700

    refactor(component top nav): search placeholder

--- ui/components/nav-top-search/index.tsx
+++ ui/components/nav-top-search/index.tsx
@@ -1,16 +1,18 @@
 import style from './style.module.css'
 
+import { t } from '@common/localization'
+
 /**
  * NavTopSearch
  * ---
- * Describe the functionality of this component. Do make sure it is properly typed as
- * it will make working with it much easier
- * !! DO NOT MERGE WITH DEFAULT COMMENTS
+ * Really this should be a universal bar with a dropdown ... but we will call it
+ * search for now
  */
 export function NavTopSearch() {
+  const searchPlaceholder = t('nav:search', 'Search')
   return (
     <div className={style.base} data-testid="nav-top-search">
-      <input id="search" name="search" type="search" />
+      <input id="search" name="search" placeholder={searchPlaceholder} type="search" />
     </div>
   )
 }
