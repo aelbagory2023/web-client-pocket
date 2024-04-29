0b4dc74d127b4a93a2dc55992489680bd712e80c
Joel <joel.kelly@me.com>
2024-04-29 11:17:58 -0700

    refactor(shared item): use css modules

--- clients/web/src/containers/home/sharedItem/sharedItem.js
+++ clients/web/src/containers/home/sharedItem/sharedItem.js
@@ -1,38 +1,11 @@
+import style from './style.module.css'
+
 import { useTranslation } from 'next-i18next'
 import { Modal, ModalBody } from 'components/modal/modal'
 import { useDispatch, useSelector } from 'react-redux'
 // import { sendSnowplowEvent } from 'connectors/snowplow/snowplow.state'
 import { ItemCard } from 'connectors/items/item-card-transitional'
 import { dismissShare } from '../../share/sharedItem.state'
-import { css } from '@emotion/css'
-
-const contextWrapper = css`
-  padding: 0 2rem;
-  h4 {
-    font-weight: 600;
-    font-size: 1rem;
-    line-height: 1.25;
-    color: var(--color-textSecondary);
-    margin-bottom: 0.5rem;
-  }
-
-  p {
-    font-size: 1.1875rem;
-    font-style: italic;
-  }
-
-  div {
-    border-left: 6px solid var(--color-dividerSecondary);
-    padding: 1rem 0 0 1rem;
-  }
-
-  .note {
-    border-color: var(--color-dividerTertiary);
-  }
-  .highlight {
-    border-color: var(--color-amber);
-  }
-`
 
 export const SharedItemInterstitial = () => {
   const { t } = useTranslation()
@@ -46,7 +19,8 @@ export const SharedItemInterstitial = () => {
   if (!sharedItem) return null
 
   const { displayItemId, context } = sharedItem
-  const { note, highlight } = context
+  const { note, highlights } = context
+  const showContext = false
   return (
     <Modal
       title={t('sharedItem:sharedItem', 'Shared Item')}
@@ -56,10 +30,12 @@ export const SharedItemInterstitial = () => {
       handleClose={closeModal}>
       <ModalBody>
         <ItemCard id={displayItemId} onOpenItem={closeModal} />
-        <div className={contextWrapper}>
-          <SharedHighlight highlight={highlight} />
-          <SharedNote note={note} />
-        </div>
+        {showContext ? (
+          <div className={style.base}>
+            <SharedHighlight highlights={highlights} />
+            <SharedNote note={note} />
+          </div>
+        ) : null}
       </ModalBody>
     </Modal>
   )
@@ -67,17 +43,21 @@ export const SharedItemInterstitial = () => {
 
 const SharedNote = ({ note }) => {
   return note ? (
-    <div className="note">
+    <div className={style.note}>
       <h4>Shared Note</h4>
       <p>{note}</p>
     </div>
   ) : null
 }
-const SharedHighlight = ({ highlight }) => {
-  return highlight ? (
-    <div className="highlight">
-      <h4>Shared Highlight</h4>
-      <p>{highlight}</p>
+const SharedHighlight = ({ highlights }) => {
+  return highlights.length ? (
+    <div className={style.highlights}>
+      <h4>Shared Highlights</h4>
+      {highlights.map((highlight) => (
+        <blockquote key={highlight.quote}>
+          <mark>{highlight.quote}</mark>
+        </blockquote>
+      ))}
     </div>
   ) : null
 }
