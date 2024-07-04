263060e5f7b1fdb3c260dd6892ce23f197ef1be8
Joel <joel.kelly@me.com>
2024-07-04 15:02:52 -0700

    test(error): update snap

--- ui/components/error-not-found/component.test.tsx
+++ ui/components/error-not-found/component.test.tsx
@@ -5,6 +5,25 @@ import { render, screen } from '@testing-library/react'
 // Components
 import { ErrorNotFound as Component } from '.'
 
+//prettier-ignore
+jest.mock('../item-media/color-planes', () => ({
+  ColorPlanes: jest.fn(() => (
+    <svg viewBox="0 0 1280 720">
+      <rect height="720" style={{ opacity: '0.3' }} width="1280"></rect>
+      <polygon points="0,525 480,584 960,443 1440,839  1920,4 640,360 0,360" style={{ opacity: '0.2' }}></polygon>
+      <polygon points="0,673 640,482 1280,71 1920,6 640,360 0,360" style={{ opacity: '0.2' }}></polygon>
+      <polygon points="0,29 640,309 1280,4 1920,248 640,360 0,360" style={{ opacity: '0.2' }}></polygon>
+      <polygon points="0,256 1920,7 640,360 0,360" style={{ opacity: '0.2' }}></polygon>
+      <polygon points="0,153 1920,4 640,360 0,360" style={{ opacity: '0.2' }}></polygon>
+    </svg>
+  ))
+}))
+
+// Optional: Clear mocks before each test if needed
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
 describe('renders ErrorFour', () => {
   it('with defaults', () => {
     const rendered = render(<Component />)
