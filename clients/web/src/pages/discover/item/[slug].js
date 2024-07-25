146fee40ab8a7b61f14bb474f1e25e0c810ef8c7
Joel <joel.kelly@me.com>
2024-07-25 13:20:21 -0700

    fix(syndicated): switch to static generation

--- clients/web/src/pages/discover/item/[slug].js
+++ clients/web/src/pages/discover/item/[slug].js
@@ -1,47 +1,59 @@
 import { SyndicatedArticle } from 'containers/syndicated-article/syndicated-article'
 import { fetchHydrationData } from 'containers/syndicated-article/syndicated-article.state'
 import { hydrateArticle } from 'containers/syndicated-article/syndicated-article.state'
-import { wrapper } from 'store'
 import { END } from 'redux-saga'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { LOCALE_COMMON } from 'common/constants'
 
-export const getServerSideProps = wrapper.getServerSideProps(
-  (store) =>
-    async ({ query, res, req, locale }) => {
-      const { dispatch, sagaTask } = store
-      const { slug, ...queryParams } = query
-      const defaultProps = {
-        ...(await serverSideTranslations(locale, [...LOCALE_COMMON])),
-        locale,
-        queryParams
+import { wrapper } from 'store'
+
+/**
+ * Server Side State Hydration
+ * This is where we are defining initial state for this page.
+ * Await on async calls here will block the page loading
+ * until they are resolved, which is fine if we need the data for
+ * SEO/Crawlers
+  --------------------------------------------------------------- */
+export const getStaticPaths = async () => {
+  return { paths: [], fallback: 'blocking' }
+}
+export const getStaticProps = wrapper.getStaticProps((store) => async ({ params, locale }) => {
+  const { dispatch, sagaTask } = store
+  const { slug, ...queryParams } = params
+  const defaultProps = {
+    ...(await serverSideTranslations(locale, [...LOCALE_COMMON])),
+    locale,
+    queryParams
+  }
+
+  // Hydrating initial state with an async request. This will block the
+  // page from loading. Do this for SEO/crawler purposes
+  const response = await fetchHydrationData({ slug })
+
+  // No article found — Check again in 12 hours minimum
+  if (!response || !response?.content) {
+    return { props: defaultProps, notFound: true, revalidate: 43200 }
+  }
+
+  if (response?.status === 'EXPIRED') {
+    return {
+      redirect: {
+        destination: response.publisherUrl,
+        statusCode: 302
       }
-
-      // Hydrating initial state with an async request. This will block the
-      // page from loading. Do this for SEO/crawler purposes
-      const baseUrl = req.headers.host
-      const response = await fetchHydrationData({ slug, baseUrl })
-
-      // No article found
-      if (!response || !response?.content) {
-        return { props: defaultProps, notFound: true }
-      }
-
-      if (response?.status === 'EXPIRED') {
-        res.writeHead(302, { Location: response.publisherUrl })
-        res.end()
-      }
-
-      // Since ssr will not wait for side effects to resolve this dispatch
-      // needs to be pure as well.
-      dispatch(hydrateArticle({ articleData: response }))
-
-      // end the saga
-      dispatch(END)
-      await sagaTask.toPromise()
-
-      return { props: defaultProps }
     }
-)
+  }
+
+  // Since ssr will not wait for side effects to resolve this dispatch
+  // needs to be pure as well.
+  dispatch(hydrateArticle({ articleData: response }))
+
+  // end the saga
+  dispatch(END)
+  await sagaTask.toPromise()
+
+  // Return page, but revalidate at most, once a day
+  return { props: defaultProps, revalidate: 86400 }
+})
 
 export default SyndicatedArticle
