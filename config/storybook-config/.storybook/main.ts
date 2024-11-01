ec53f8ad2f7bba6f04dc42b81a4bce91ec888b53
Joel <joel.kelly@me.com>
2024-11-01 14:51:12 -0700

    chore(dependencies): update to latest

--- config/storybook-config/.storybook/main.ts
+++ config/storybook-config/.storybook/main.ts
@@ -26,23 +26,5 @@ const config: StorybookConfig = {
   features: {
     experimentalRSC: true
   }
-  // webpackFinal: async (config: any): Promise<any> => {
-  //   const alias = {
-  //     mock: path.join(__dirname, rootDirectory, '__mocks__/_data'),
-  //     mocks: path.join(__dirname, rootDirectory, '__mocks__'),
-  //     media: path.join(__dirname, rootDirectory, 'public/static'),
-  //     common: path.join(__dirname, rootDirectory, 'src/common'),
-  //     containers: path.join(__dirname, rootDirectory, 'src/containers'),
-  //     components: path.join(__dirname, rootDirectory, 'src/components'),
-  //     connectors: path.join(__dirname, rootDirectory, 'src/connectors'),
-  //     actions$: path.join(__dirname, rootDirectory, 'src/actions.js'),
-  //     store$: path.join(__dirname, rootDirectory, 'src/store.js'),
-  //     layouts: path.join(__dirname, rootDirectory, 'src/containers/_layouts'),
-  //     '@braze/web-sdk': path.join(__dirname, rootDirectory, '__mocks__/braze-sdk.js')
-  //   }
-  //   config.resolve.alias = { ...config.resolve.alias, ...alias }
-
-  //   return config
-  // }
 }
 export default config
