4125032acaca99023f1d5df9b4362770f6ec1728
Joel <joel.kelly@me.com>
2024-12-06 10:05:18 -0800

    chore(test wraper): simplify declaration

--- config/jest-config/wrapper.tsx
+++ config/jest-config/wrapper.tsx
@@ -1,12 +1,6 @@
 import { i18n } from './setup/test-i18n'
 import { I18nextProvider } from 'react-i18next'
 
-import type { PropsWithChildren, ReactElement } from 'react'
-
-export function localeWrapper({ children }: PropsWithChildren<NonNullable<unknown>>): JSX.Element {
-  return (
-    <I18nextProvider i18n={i18n}>
-      {children}
-    </I18nextProvider>
-  )
+export function localeWrapper({ children }: { children: React.ReactNode }) {
+  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
 }
