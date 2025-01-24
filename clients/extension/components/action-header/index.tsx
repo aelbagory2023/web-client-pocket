afd4c9cca2384b1d36e87387a8434164e1df03e2
Joel <joel.kelly@me.com>
2025-01-23 18:12:22 -0800

    feat(extension): add logo back

--- clients/extension/components/action-header/index.tsx
+++ clients/extension/components/action-header/index.tsx
@@ -1,14 +1,16 @@
 import style from './style.module.css'
 import { OverflowMenuIcon } from '../icons/OverflowMenuIcon'
-import { SaveFilledIcon } from '../icons/SaveFilledIcon'
-import { SaveIcon } from '../icons/SaveIcon'
+import { PocketLogo } from '../icons/Pocket'
+// import { SaveFilledIcon } from '../icons/SaveFilledIcon'
+// import { SaveIcon } from '../icons/SaveIcon'
 import { SavedLoader } from '../saved-loader'
 
 export function ExtensionHeader({ saveStatus }: { saveStatus?: string }) {
   const loaded = saveStatus !== 'saving'
   return (
     <header className={style.header}>
-      {loaded ? <SaveFilledIcon className={style.icon} /> : <SaveIcon className={style.icon} />}
+      {/* {loaded ? <SaveFilledIcon className={style.icon} /> : <SaveIcon className={style.icon} />} */}
+      <PocketLogo />
       {loaded ? <div /> : <SavedLoader />}
       <OverflowMenuIcon />
     </header>
