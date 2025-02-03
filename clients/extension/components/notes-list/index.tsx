c8d8ade5ae7bad63812a758e24846de953e644ea
Joel <joel.kelly@me.com>
2025-02-03 14:52:56 -0800

    feat(extension): ui for delete confirm

--- clients/extension/components/notes-list/index.tsx
+++ clients/extension/components/notes-list/index.tsx
@@ -1,5 +1,6 @@
 import style from './style.module.css'
 import { CrossIcon } from '@ui/icons/CrossIcon'
+import { useState, useRef } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 // Types
@@ -18,11 +19,21 @@ export function NotesList({ notes }: { notes?: NoteEdge[] }) {
 }
 
 function Note({ note }: { note?: NoteEdge }) {
+  const [confirmDelete, setConfirmDelete] = useState<boolean>(false)
+  const noteRef = useRef<HTMLDivElement>(null)
+
   const node = note?.node
   const title = node?.title
   const contentPreview = node?.contentPreview as string
+
+  const handleDeleteClick = () => {
+    if (noteRef.current) noteRef.current.scrollIntoView({ behavior: 'smooth' })
+    setConfirmDelete(true)
+  }
+  const handleCancelClick = () => setConfirmDelete(false)
+
   return contentPreview ? (
-    <div className={style.container}>
+    <div className={`${style.container} ${confirmDelete && style.active}`} ref={noteRef}>
       <div className={style.note}>
         {title ? <h3>{title}</h3> : null}
         <ReactMarkdown allowedElements={['div']} unwrapDisallowed={true}>
@@ -30,14 +41,25 @@ function Note({ note }: { note?: NoteEdge }) {
         </ReactMarkdown>
       </div>
       <div className={style.actions}>
-        <button>
-          <CrossIcon />
-        </button>
+        {!confirmDelete ? (
+          <button onClick={handleDeleteClick}>
+            <CrossIcon />
+          </button>
+        ) : null}
       </div>
+      {confirmDelete ? <NoteDeleteConfirm handleCancelClick={handleCancelClick} /> : null}
     </div>
   ) : null
 }
 
+function NoteDeleteConfirm({ handleCancelClick }: { handleCancelClick: () => void }) {
+  return (
+    <div className={style.confirm}>
+      <button>Delete this note</button>
+      <button onClick={handleCancelClick}>Cancel</button>
+    </div>
+  )
+}
 function NoNotes() {
   return <div className={style.noNotes}>Add your first note to this page</div>
 }
